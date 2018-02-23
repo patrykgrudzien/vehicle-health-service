d4fc1559c66cc5631771766545889b913663db9d
patrykgrudzien <patryk.grudzien.91@gmail.com>
2018-02-23 14:30:38 +0100

    Prepared app structure. Created first simple controller to check functionality.

---
+++ backend/src/main/java/me/grudzien/patryk/controller/HelloWorldController.java
@@ -0,0 +1,21 @@
+package me.grudzien.patryk.controller;
+
+import lombok.extern.log4j.Log4j;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/api")
+@Log4j
+public class HelloWorldController {
+
+	private static final String HELLO_TEXT = "Hello World from Full-Stack Web App";
+
+	@RequestMapping("/hello")
+	public @ResponseBody String helloWorld() {
+		log.info("GET called on /hello resource");
+		return HELLO_TEXT;
+	}
+}
