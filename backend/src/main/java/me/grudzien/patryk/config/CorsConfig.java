d1af99c64f04b0d1840957da9a86f9411e652b43
PatrykGrudzien <patryk.grudzien.91@gmail.com>
2018-02-24 20:56:35 +0100

    Format imports.

--- backend/src/main/java/me/grudzien/patryk/config/CorsConfig.java
+++ backend/src/main/java/me/grudzien/patryk/config/CorsConfig.java
@@ -1,12 +1,12 @@
 package me.grudzien.patryk.config;
 
-import me.grudzien.patryk.constants.CorsOrigins;
-
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
+
+import me.grudzien.patryk.constants.CorsOrigins;
 
 @Configuration
 public class CorsConfig {
