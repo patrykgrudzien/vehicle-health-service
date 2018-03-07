f4862934d4b6835f95af0ad0e05e158959a8f3aa
patrykgrudzien <patryk.grudzien.91@gmail.com>
2018-03-07 15:40:07 +0100

    Enable CORS configuration for dev related spring profiles.

--- backend/src/main/java/me/grudzien/patryk/controller/CorsController.java
+++ backend/src/main/java/me/grudzien/patryk/controller/CorsController.java
@@ -1,8 +1,13 @@
 package me.grudzien.patryk.controller;
 
+import lombok.extern.log4j.Log4j;
+
+import org.springframework.context.annotation.Profile;
 import org.springframework.web.bind.annotation.CrossOrigin;
 
 import me.grudzien.patryk.constants.CorsOrigins;
 
+@Log4j
 @CrossOrigin(origins = CorsOrigins.FRONTEND_MODULE)
+@Profile({"dev-home", "dev-office"})
 public class CorsController {}
