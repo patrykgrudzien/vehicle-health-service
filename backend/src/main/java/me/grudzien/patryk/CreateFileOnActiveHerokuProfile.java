cc54d05a823e9970d5b66f1fe0fce2b4a8346a64
PatrykGrudzien <patryk.grudzien.91@gmail.com>
2018-03-14 20:38:42 +0100

    Improved generation of "heroku-deployment-enabled" file.

--- backend/src/main/java/me/grudzien/patryk/CreateFileOnActiveHerokuProfile.java
+++ backend/src/main/java/me/grudzien/patryk/CreateFileOnActiveHerokuProfile.java
@@ -17,16 +17,20 @@ import java.io.IOException;
 public class CreateFileOnActiveHerokuProfile {
 
 	private static final String BACKEND_MODULE_NAME = "backend";
+	private static final String HEROKU_DEPLOYMENT_PROFILE_NAME = "heroku-deployment";
 
 	public static void main(final String[] args) {
 		try {
 			Files.readLines(new FileSystemResource(resolveApplicationYmlPath()).getFile(), Charsets.UTF_8)
 			     .stream()
-			     .peek(System.out::println)
 			     // filtering to find active profile
 			     .filter(line -> line.contains("active: "))
+			     // display in console name of active profile
+			     .peek(activeProfileName -> System.out.println("ACTIVE PROFILE >>>> " + activeProfileName))
 			     // taking only active profile name after ": " character
 			     .map(line -> line.substring(line.indexOf(':') + 1).trim())
+			     // create profile only for "heroku-deployment" profile
+			     .filter(profileName -> profileName.equals(HEROKU_DEPLOYMENT_PROFILE_NAME))
 			.findFirst()
 			// if present generate file which will fire Heroku Maven Plugin and appropriate profiles
 			.ifPresent(CreateFileOnActiveHerokuProfile::createFile);
