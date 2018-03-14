8fd92116d3661705daa95abfc02bf6dacdcb2f09
PatrykGrudzien <patryk.grudzien.91@gmail.com>
2018-03-15 00:18:25 +0100

    Another maven configuration for Heroku best support (still files not recognized). Trying deploy with static resources.

--- backend/src/main/java/me/grudzien/patryk/CreateFileOnActiveHerokuProfile.java
+++ backend/src/main/java/me/grudzien/patryk/CreateFileOnActiveHerokuProfile.java
@@ -5,17 +5,20 @@ import lombok.extern.log4j.Log4j;
 import org.springframework.core.io.FileSystemResource;
 
 import com.google.common.base.Charsets;
+import com.google.common.collect.Lists;
 import com.google.common.io.Files;
 
 import java.io.IOException;
+import java.util.List;
 
 /**
- * This class is used by exec-maven-plugin which is gonna execute this main() method and create file which is a trigger for
+ * This class is used by exec-maven-plugin which is gonna execute this main() method and create files which are triggers for
  * heroku-maven-plugin to start deployment process on Heroku.
  */
 @Log4j
 public class CreateFileOnActiveHerokuProfile {
 
+	private static final String FRONTEND_MODULE_NAME = "frontend";
 	private static final String BACKEND_MODULE_NAME = "backend";
 	private static final String HEROKU_DEPLOYMENT_PROFILE_NAME = "heroku-deployment";
 
@@ -39,34 +42,56 @@ public class CreateFileOnActiveHerokuProfile {
 		}
 	}
 
-	private static String createFile(final String activeProfileName) {
-		final String successMessage = "File >>>> " + activeProfileName + "-enabled <<<< has been created!";
-		try {
-			// heroku-deployment-enabled
-			Files.touch(new FileSystemResource(resolveHerokuDeploymentEnabledFileOutputPath(activeProfileName)).getFile());
-			log.info(successMessage);
-		} catch (final IOException exception) {
-			log.error(exception.getMessage());
-		}
-		return "createFile() method executed.";
-	}
-
 	private static String resolveApplicationYmlPath() throws IOException {
 		final FileSystemResource fileSystemResource = new FileSystemResource("src/main/resources/application.yml");
 		final String path = fileSystemResource.getURI().getPath();
 
-		if (path.contains(BACKEND_MODULE_NAME) || path.equals("")) {
+		if (path.contains(BACKEND_MODULE_NAME)) {
 			return "src/main/resources/application.yml";
 		} else {
 			return BACKEND_MODULE_NAME + "/src/main/resources/application.yml";
 		}
 	}
 
-	private static String resolveHerokuDeploymentEnabledFileOutputPath(final String activeProfileName) throws IOException {
-		if (new FileSystemResource("").getURI().getPath().contains(BACKEND_MODULE_NAME)) {
-			return "../" + activeProfileName + "-enabled";
-		} else {
-			return activeProfileName + "-enabled";
+	private static List<String> resolveHerokuDeploymentEnabledFilesOutputPaths(final String activeProfileName) {
+
+		String projectPath = "";
+		try {
+			projectPath = new FileSystemResource("").getURI().getPath();
+			log.info("Project path found: " + projectPath);
+		} catch (final IOException exception) {
+			log.error("Cannot find project path !!!");
 		}
+		final List<String> generatedFilesPaths = Lists.newArrayList();
+
+		if (projectPath.contains(FRONTEND_MODULE_NAME)) {
+			generatedFilesPaths.add(activeProfileName + "-" + FRONTEND_MODULE_NAME + "-enabled");
+			generatedFilesPaths.add("../" + BACKEND_MODULE_NAME + "/" + activeProfileName + "-" + BACKEND_MODULE_NAME + "-enabled");
+		}
+		if (projectPath.contains(BACKEND_MODULE_NAME)) {
+			generatedFilesPaths.add(activeProfileName + "-" + BACKEND_MODULE_NAME + "-enabled");
+			generatedFilesPaths.add("../" + FRONTEND_MODULE_NAME + "/" + activeProfileName + "-" + FRONTEND_MODULE_NAME + "-enabled");
+		}
+		else {
+			generatedFilesPaths.add(FRONTEND_MODULE_NAME + "/" + activeProfileName + "-" + FRONTEND_MODULE_NAME + "-enabled");
+			generatedFilesPaths.add(BACKEND_MODULE_NAME + "/" + activeProfileName + "-" + BACKEND_MODULE_NAME + "-enabled");
+		}
+		return generatedFilesPaths;
+	}
+
+	private static String createFile(final String activeProfileName) {
+		try {
+			resolveHerokuDeploymentEnabledFilesOutputPaths(activeProfileName).forEach(path -> {
+				try {
+					Files.touch(new FileSystemResource(path).getFile());
+					log.info("File >>>> " + path + " <<<< has been created!");
+				} catch (final IOException exception) {
+					log.error(exception.getMessage());
+				}
+			});
+		} finally {
+			log.info("Files(s) created. TASK COMPLETED.");
+		}
+		return "createFile() method executed.";
 	}
 }
--- backend/src/main/java/me/grudzien/patryk/config/cors/CorsConfig.java
+++ backend/src/main/java/me/grudzien/patryk/config/cors/CorsConfig.java
@@ -1,32 +1,32 @@
-package me.grudzien.patryk.config.cors;
-
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.context.annotation.Bean;
-import org.springframework.context.annotation.Configuration;
-import org.springframework.web.servlet.config.annotation.CorsRegistry;
-import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
-import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
-
-import me.grudzien.patryk.config.custom.CustomApplicationProperties;
-
-@Configuration
-public class CorsConfig {
-
-	private final CustomApplicationProperties customApplicationProperties;
-
-	@Autowired
-	public CorsConfig(final CustomApplicationProperties customApplicationProperties) {
-		this.customApplicationProperties = customApplicationProperties;
-	}
-
-	@Bean
-	public WebMvcConfigurer corsConfigurer() {
-		return new WebMvcConfigurerAdapter() {
-			@Override
-			public void addCorsMappings(final CorsRegistry registry) {
-				registry.addMapping("/**")
-				        .allowedOrigins(customApplicationProperties.getCorsOrigins().getFrontEndModule());
-			}
-		};
-	}
-}
+//package me.grudzien.patryk.config.cors;
+//
+//import org.springframework.beans.factory.annotation.Autowired;
+//import org.springframework.context.annotation.Bean;
+//import org.springframework.context.annotation.Configuration;
+//import org.springframework.web.servlet.config.annotation.CorsRegistry;
+//import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+//import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
+//
+//import me.grudzien.patryk.config.custom.CustomApplicationProperties;
+//
+//@Configuration
+//public class CorsConfig {
+//
+//	private final CustomApplicationProperties customApplicationProperties;
+//
+//	@Autowired
+//	public CorsConfig(final CustomApplicationProperties customApplicationProperties) {
+//		this.customApplicationProperties = customApplicationProperties;
+//	}
+//
+//	@Bean
+//	public WebMvcConfigurer corsConfigurer() {
+//		return new WebMvcConfigurerAdapter() {
+//			@Override
+//			public void addCorsMappings(final CorsRegistry registry) {
+//				registry.addMapping("/**")
+//				        .allowedOrigins(customApplicationProperties.getCorsOrigins().getFrontEndModule());
+//			}
+//		};
+//	}
+//}
