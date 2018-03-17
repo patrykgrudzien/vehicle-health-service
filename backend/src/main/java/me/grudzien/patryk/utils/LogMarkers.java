66e11372adba3796ce2600cd646b9a360ed704c0
PatrykGrudzien <patryk.grudzien.91@gmail.com>
2018-03-17 03:06:00 +0100

    Added LogMarkers class and improvement logging in one small app part.

---
+++ backend/src/main/java/me/grudzien/patryk/utils/LogMarkers.java
@@ -0,0 +1,17 @@
+package me.grudzien.patryk.utils;
+
+import org.apache.logging.log4j.Marker;
+import org.apache.logging.log4j.MarkerManager;
+
+/**
+ * Class which stores different Markers (no need to extends it).
+ */
+public final class LogMarkers {
+
+	// disabling class object creation
+	private LogMarkers() {}
+
+	public static final Marker FLOW_MARKER = MarkerManager.getMarker("FLOW");
+	public static final Marker CONTROLLER_MARKER = MarkerManager.getMarker("CONTROLLER");
+	public static final Marker METHOD_INVOCATION_MARKER = MarkerManager.getMarker("METHOD_INVOCATION");
+}
