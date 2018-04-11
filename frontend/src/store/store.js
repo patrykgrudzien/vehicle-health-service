d2d79bc021324df80fc7af1c3760f98391f324f9
patrykgrudzien <patryk.grudzien.91@gmail.com>
2018-04-11 09:09:13 +0200

    Added vuex-persistedstate and js-cookie modules.

--- frontend/src/store/store.js
+++ frontend/src/store/store.js
@@ -1,10 +1,13 @@
 import Vue from 'vue';
 import Vuex from 'vuex';
-Vue.use(Vuex);
-
 import getters from './getters';
 import actions from './actions';
 import mutations from './mutations';
+
+import createPersistedState from 'vuex-persistedstate';
+import * as Cookies from 'js-cookie';
+
+Vue.use(Vuex);
 
 const state = {
   logged: localStorage.getItem('token'),
@@ -17,5 +20,19 @@ export default new Vuex.Store({
   state,
   getters,
   actions,
-  mutations
-})
+  mutations,
+  plugins: [
+    createPersistedState({
+      getState: () => {
+        if (Cookies.getJSON('lang')) {
+          state.lang = Cookies.getJSON('lang');
+        } else {
+          state.lang = 'en';
+        }
+      },
+      setState: () => {
+        Cookies.set('lang', state.lang);
+      }
+    })
+  ]
+});
