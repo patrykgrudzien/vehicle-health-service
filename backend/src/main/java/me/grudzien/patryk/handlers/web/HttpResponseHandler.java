e2546a13b4458c1ada37e8e71c56f04051d10219
patrykgrudzien <patryk.grudzien.91@gmail.com>
2018-04-26 17:28:23 +0200

    Small refactoring.

--- backend/src/main/java/me/grudzien/patryk/handlers/web/HttpResponseHandler.java
+++ backend/src/main/java/me/grudzien/patryk/handlers/web/HttpResponseHandler.java
@@ -18,7 +18,7 @@ import me.grudzien.patryk.utils.log.LogMarkers;
 @Component
 public class HttpResponseHandler {
 
-	public static final String SECURED_RESOURCE_CODE = "SECURED-RESOURCE";
+	public static final String SECURED_RESOURCE_CODE = "SECURED";
 	public static final String CONTENT_TYPE_KEY = "Content-Type";
 	public static final String CONTENT_TYPE_VALUE = "application/json;charset=UTF-8";
 
