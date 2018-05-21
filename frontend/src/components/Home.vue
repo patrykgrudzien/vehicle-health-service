997be81490f14b1dad00fccf0366f0e826c31e05
patrykgrudzien <patryk.grudzien.91@gmail.com>
2018-05-21 13:31:18 +0200

    Returned to old home layout with enhancements.

--- frontend/src/components/Home.vue
+++ frontend/src/components/Home.vue
@@ -1,13 +1,60 @@
 <template>
+  <!-- full size home-background -->
   <v-container id="home-background" fluid fill-height>
-    <!-- full size home-background -->
+    <v-layout align-center justify-center>
+      <v-flex xs12 sm10 md8 xl6>
+        <v-card id="cards-container" flat disabled>
+          <v-container grid-list-xs>
+            <v-layout>
+              <v-flex>
+                <v-card-text class="text-xs-center display-2">{{ $t('background-text-1') }}</v-card-text>
+              </v-flex>
+            </v-layout>
+          </v-container>
+          <v-container grid-list-xs>
+            <v-layout>
+              <v-flex xs6>
+                <v-card-text class="headline text-xs-center">{{ $t('background-text-2') }}</v-card-text>
+              </v-flex>
+              <v-flex xs6>
+                <v-card-text class="headline text-xs-center">{{ $t('background-text-3') }}</v-card-text>
+              </v-flex>
+            </v-layout>
+          </v-container>
+          <v-container grid-list-xs>
+            <v-layout>
+              <v-flex xs6>
+                <v-card-text class="headline text-xs-center">{{ $t('background-text-4') }}</v-card-text>
+              </v-flex>
+              <v-flex xs6>
+                <v-card-text class="headline text-xs-center">{{ $t('background-text-5') }}</v-card-text>
+              </v-flex>
+            </v-layout>
+          </v-container>
+        </v-card>
+      </v-flex>
+    </v-layout>
   </v-container>
 </template>
 
+<script>
+  export default {
+    data() {
+      return {}
+    }
+  }
+</script>
+
 <style scoped>
+  #cards-container {
+    background: rgba(60, 60, 60, 0.7);
+    font-family: 'Arsenal', sans-serif;
+  }
+
   #home-background {
-    background: url('../../static/engine-1.jpg') no-repeat center center fixed;
+    background: url('../../static/engine-5.jpg') no-repeat center center fixed;
     background-size: contain;
+    box-shadow: inset 0 0 0 2000px rgba(48, 48, 48, 0.55);
   }
 
   @media screen and (max-width: 768px) and (max-height: 1024px) {
@@ -15,7 +62,7 @@
       width: 768px;
       height: 100%;
       background-size: cover;
-      box-shadow: inset 0 0 0 2000px rgba(48, 48, 48, 0.4);
+      box-shadow: inset 0 0 0 2000px rgba(48, 48, 48, 0.55);
     }
   }
 
@@ -24,7 +71,7 @@
       width: 1024px;
       height: 100%;
       background-size: cover;
-      box-shadow: inset 0 0 0 2000px rgba(48, 48, 48, 0.4);
+      box-shadow: inset 0 0 0 2000px rgba(48, 48, 48, 0.55);
     }
   }
 
@@ -33,7 +80,7 @@
       width: 1366px;
       height: 100%;
       background-size: cover;
-      box-shadow: inset 0 0 0 2000px rgba(48, 48, 48, 0.4);
+      box-shadow: inset 0 0 0 2000px rgba(48, 48, 48, 0.55);
     }
   }
 
@@ -42,7 +89,7 @@
       width: 1366px;
       height: 100%;
       background-size: cover;
-      box-shadow: inset 0 0 0 2000px rgba(48, 48, 48, 0.4);
+      box-shadow: inset 0 0 0 2000px rgba(48, 48, 48, 0.55);
     }
   }
 
@@ -51,7 +98,7 @@
       width: 1080px;
       height: 100%;
       background-size: cover;
-      box-shadow: inset 0 0 0 2000px rgba(48, 48, 48, 0.4);
+      box-shadow: inset 0 0 0 2000px rgba(48, 48, 48, 0.55);
     }
   }
 
@@ -60,7 +107,7 @@
       width: 1920px;
       height: 100%;
       background-size: cover;
-      box-shadow: inset 0 0 0 2000px rgba(48, 48, 48, 0.4);
+      box-shadow: inset 0 0 0 2000px rgba(48, 48, 48, 0.55);
     }
   }
 
@@ -69,7 +116,7 @@
       width: 100%;
       height: 100%;
       background-size: cover;
-      box-shadow: inset 0 0 0 2000px rgba(48, 48, 48, 0.4);
+      box-shadow: inset 0 0 0 2000px rgba(48, 48, 48, 0.55);
     }
   }
 </style>
