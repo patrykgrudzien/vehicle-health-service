22d34c63631c64260dda361a19ba6b64c5de3f5e
patrykgrudzien <patryk.grudzien.91@gmail.com>
2018-05-25 20:43:22 +0200

    Added names to vue routes and auth meta data.

--- frontend/src/router/allRoutes.js
+++ frontend/src/router/allRoutes.js
@@ -4,39 +4,54 @@ import AboutMe             from '../components/AboutMe';
 import RegistrationForm    from '../components/RegistrationForm';
 import ConfirmRegistration from '../components/ConfirmRegistration';
 import LoginForm           from '../components/LoginForm';
+import MainBoard           from '../components/MainBoard';
 import componentsPaths     from '../componentsPaths';
 
 export default [
   {
+    name: 'Home',
     path: componentsPaths.home,
     component: Home
   },
   {
+    name: 'ServerCheck',
     path: componentsPaths.serverCheck,
     component: ServerCheck
   },
   {
+    name: 'AboutMe',
     path: componentsPaths.aboutMe,
     component: AboutMe
   },
   {
+    name: 'RegistrationForm',
     path: componentsPaths.registrationForm,
     component: RegistrationForm
   },
   {
+    name: 'ConfirmRegistration',
     path: componentsPaths.confirmRegistration,
     component: ConfirmRegistration
   },
   {
+    name: 'LoginForm',
     path: componentsPaths.loginForm,
     component: LoginForm
   },
   {
+    name: 'LoginForm-login-failed',
     path: componentsPaths.loginFailed,
     component: LoginForm
   },
   {
+    name: 'LoginForm-logout-successful-wildcard',
     path: componentsPaths.logoutSuccessfulWildcard,
     component: LoginForm
+  },
+  {
+    name: 'MainBoard',
+    path: componentsPaths.mainBoard,
+    component: MainBoard,
+    meta: {requiresAuth: true}
   }
 ];
