f9c1f2b068bee8ebaebb0d4c9aaaf09a1846b7e1
patrykgrudzien <patryk.grudzien.91@gmail.com>
2018-06-14 09:36:29 +0200

    Improved CorsConfig.

--- backend/src/main/java/me/grudzien/patryk/config/cors/CorsConfig.java
+++ backend/src/main/java/me/grudzien/patryk/config/cors/CorsConfig.java
@@ -1,24 +1,14 @@
 package me.grudzien.patryk.config.cors;
 
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.Profile;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
-import me.grudzien.patryk.config.custom.CustomApplicationProperties;
-
 @Configuration
 @Profile({"dev-home", "dev-office"})
 public class CorsConfig {
-
-	private final CustomApplicationProperties customApplicationProperties;
-
-	@Autowired
-	public CorsConfig(final CustomApplicationProperties customApplicationProperties) {
-		this.customApplicationProperties = customApplicationProperties;
-	}
 
 	@Bean
 	public WebMvcConfigurer corsConfigurer() {
@@ -26,7 +16,10 @@ public class CorsConfig {
 			@Override
 			public void addCorsMappings(final CorsRegistry registry) {
 				registry.addMapping("/**")
-				        .allowedOrigins(customApplicationProperties.getCorsOrigins().getFrontEndModule());
+				        .allowedMethods("GET", "POST", "PUT", "DELETE")
+				        .allowedOrigins("*")
+				        .allowedHeaders("*")
+				        .exposedHeaders("X-Auth-Token", "Authorization", "Access-Control-Allow-Origin", "Access-Control-Allow-Credentials");
 			}
 		};
 	}
