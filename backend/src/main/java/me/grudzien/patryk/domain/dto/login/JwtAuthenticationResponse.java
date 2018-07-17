0e699361c4f7e452aaf33136a9836ba5ac65b486
patrykgrudzien <patryk.grudzien.91@gmail.com>
2018-07-17 16:11:51 +0200

    Added builder.

--- backend/src/main/java/me/grudzien/patryk/domain/dto/login/JwtAuthenticationResponse.java
+++ backend/src/main/java/me/grudzien/patryk/domain/dto/login/JwtAuthenticationResponse.java
@@ -1,5 +1,6 @@
 package me.grudzien.patryk.domain.dto.login;
 
+import lombok.Builder;
 import lombok.Getter;
 import lombok.RequiredArgsConstructor;
 
@@ -7,6 +8,7 @@ import java.io.Serializable;
 
 @Getter
 @RequiredArgsConstructor
+@Builder(builderMethodName = "Builder")
 public class JwtAuthenticationResponse implements Serializable {
 
 	private static final long serialVersionUID = 6971598280333003305L;
