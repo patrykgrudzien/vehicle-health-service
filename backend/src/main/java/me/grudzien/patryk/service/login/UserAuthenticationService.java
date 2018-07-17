5879367f123dcfa633a99435b39a00e5b96f492a
patrykgrudzien <patryk.grudzien.91@gmail.com>
2018-07-17 16:12:25 +0200

    Started implementing controller and service for refresh token.

--- backend/src/main/java/me/grudzien/patryk/service/login/UserAuthenticationService.java
+++ backend/src/main/java/me/grudzien/patryk/service/login/UserAuthenticationService.java
@@ -2,9 +2,13 @@ package me.grudzien.patryk.service.login;
 
 import org.springframework.mobile.device.Device;
 
+import javax.servlet.http.HttpServletRequest;
+
 import me.grudzien.patryk.domain.dto.login.JwtAuthenticationRequest;
 
 public interface UserAuthenticationService {
 
 	String authenticateAndGenerateToken(final JwtAuthenticationRequest authenticationRequest, final Device device);
+
+	String refreshAuthenticationToken(final HttpServletRequest request);
 }
