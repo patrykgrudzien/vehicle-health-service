5e6163995dc7c11107888882b7f64b66c1efd046
patrykgrudzien <patryk.grudzien.91@gmail.com>
2018-07-29 13:36:02 +0200

    Added preauthorize annotation to VehicleController with RequestParamPathVariableGuard.

--- backend/src/main/java/me/grudzien/patryk/controller/vehicle/VehicleController.java
+++ backend/src/main/java/me/grudzien/patryk/controller/vehicle/VehicleController.java
@@ -3,6 +3,7 @@ package me.grudzien.patryk.controller.vehicle;
 import lombok.extern.log4j.Log4j2;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PutMapping;
@@ -40,6 +41,7 @@ public class VehicleController {
 	}
 
 	@GetMapping("/vehicle/get-current-mileage/{ownerEmailAddress}")
+	@PreAuthorize("@requestParamPathVariableGuard.isUserEmailAuthenticated(#ownerEmailAddress)")
 	public Long getVehicleCurrentMileage(@PathVariable("ownerEmailAddress") final String ownerEmailAddress,
 	                                     @SuppressWarnings("unused") final WebRequest webRequest) {
 		return vehicleService.getVehicleCurrentMileage(ownerEmailAddress);
