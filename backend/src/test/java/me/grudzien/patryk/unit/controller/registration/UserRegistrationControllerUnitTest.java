71521e0aa57e69181b0de20c8e58672a374a8b37
patrykgrudzien <patryk.grudzien.91@gmail.com>
2018-08-24 13:46:45 +0200

    Renamed test classes because maven scans for ""..Test" keyword.

--- backend/src/test/java/me/grudzien/patryk/unit/controller/registration/UserRegistrationControllerUnitTest.java
+++ backend/src/test/java/me/grudzien/patryk/unit/controller/registration/UserRegistrationControllerUnitTest.java
@@ -3,10 +3,12 @@ package me.grudzien.patryk.unit.controller.registration;
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
 import org.springframework.test.context.junit4.SpringRunner;
 
+import org.junit.Ignore;
 import org.junit.runner.RunWith;
 
 import me.grudzien.patryk.controller.registration.UserRegistrationController;
 
+@Ignore
 @RunWith(SpringRunner.class)
 @WebMvcTest(controllers = UserRegistrationController.class)
-public class UserRegistrationControllerUT {}
+public class UserRegistrationControllerUnitTest {}
