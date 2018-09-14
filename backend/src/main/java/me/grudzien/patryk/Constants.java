9d36ac9a8304595de2779b9913346f8e3cf068ff
patrykgrudzien <patryk.grudzien.91@gmail.com>
2018-09-14 12:35:59 +0200

    New constants.

--- backend/src/main/java/me/grudzien/patryk/Constants.java
+++ backend/src/main/java/me/grudzien/patryk/Constants.java
@@ -4,15 +4,27 @@ import me.grudzien.patryk.config.custom.CustomApplicationProperties;
 
 public interface Constants {
 
-	CustomApplicationProperties customApplicationProperties = new CustomApplicationProperties();
+	CustomApplicationProperties customProps = new CustomApplicationProperties();
 
 	interface Endpoints {
-		String AUTH = customApplicationProperties.getEndpoints().getAuthentication().getRoot();
-		String REGISTRATION = customApplicationProperties.getEndpoints().getRegistration().getRoot();
-		String REFRESH_TOKEN = customApplicationProperties.getEndpoints().getAuthentication().getRefreshToken();
+		String AUTH = customProps.getEndpoints().getAuthentication().getRoot();
+		String REGISTRATION = customProps.getEndpoints().getRegistration().getRoot();
+		String REFRESH_TOKEN = customProps.getEndpoints().getAuthentication().getRefreshToken();
 	}
 
 	interface OAuth2 {
 		String LOGIN_PAGE = "http://localhost:8080/login";
+		int SHORT_LIVED_MILLIS = customProps.getJwt().getShortLivedMillis();
+	}
+
+	interface FrontendRoutes {
+		String ABOUT_ME = "/about-me";
+		String REGISTRATION_FORM = "/registration-form";
+		String REGISTRATION_CONFIRMED = "/registration-confirmed";
+		String REGISTRATION_CONFIRMED_WILDCARD = "/registration-confirmed/**";
+		String LOGIN = "/login";
+		String MAIN_BOARD = "/main-board";
+		String MAIN_BOARD_WILDCARD = "/main-board/**";
+		String AUTHENTICATION_REQUIRED = "/authentication-required";
 	}
 }
