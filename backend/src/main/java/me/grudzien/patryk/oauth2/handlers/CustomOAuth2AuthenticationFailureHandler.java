b2e6eeb6d34898b2c717f4eafa5aaaf85e6a1df1
patrykgrudzien <patryk.grudzien.91@gmail.com>
2018-09-20 12:15:13 +0200

    CustomOAuth2AuthenticationFailureHandler & CustomOAuth2AuthenticationSuccessHandler changes.

--- backend/src/main/java/me/grudzien/patryk/oauth2/handlers/CustomOAuth2AuthenticationFailureHandler.java
+++ backend/src/main/java/me/grudzien/patryk/oauth2/handlers/CustomOAuth2AuthenticationFailureHandler.java
@@ -15,24 +15,33 @@ import javax.servlet.http.HttpServletResponse;
 
 import java.io.IOException;
 
+import me.grudzien.patryk.PropertiesKeeper;
 import me.grudzien.patryk.oauth2.repository.CacheBasedOAuth2AuthorizationRequestRepository;
+
+import static me.grudzien.patryk.utils.log.LogMarkers.OAUTH2_MARKER;
 
 @Log4j2
 @Component
 public class CustomOAuth2AuthenticationFailureHandler extends SimpleUrlAuthenticationFailureHandler {
 
 	private final CacheBasedOAuth2AuthorizationRequestRepository cacheBasedOAuth2AuthorizationRequestRepository;
+	private final PropertiesKeeper propertiesKeeper;
 
 	@Autowired
-	public CustomOAuth2AuthenticationFailureHandler(final CacheBasedOAuth2AuthorizationRequestRepository cacheBasedOAuth2AuthorizationRequestRepository) {
+	public CustomOAuth2AuthenticationFailureHandler(final CacheBasedOAuth2AuthorizationRequestRepository cacheBasedOAuth2AuthorizationRequestRepository,
+	                                                final PropertiesKeeper propertiesKeeper) {
 		Preconditions.checkNotNull(cacheBasedOAuth2AuthorizationRequestRepository, "cacheBasedOAuth2AuthorizationRequestRepository cannot be null!");
+		Preconditions.checkNotNull(propertiesKeeper, "propertiesKeeper cannot be null!");
 		this.cacheBasedOAuth2AuthorizationRequestRepository = cacheBasedOAuth2AuthorizationRequestRepository;
+		this.propertiesKeeper = propertiesKeeper;
 	}
 
 	@Override
 	public void onAuthenticationFailure(final HttpServletRequest request, final HttpServletResponse response, final AuthenticationException exception)
 			throws IOException, ServletException {
+		log.info(OAUTH2_MARKER, ">>>> OAUTH2 <<<< onAuthenticationFailure()");
 		cacheBasedOAuth2AuthorizationRequestRepository.evictOAuth2AuthorizationRequestCache();
+		super.setDefaultFailureUrl(propertiesKeeper.oAuth2().FAILURE_TARGET_URL);
 		super.onAuthenticationFailure(request, response, exception);
 	}
 }
--- backend/src/main/java/me/grudzien/patryk/oauth2/handlers/CustomOAuth2AuthenticationSuccessHandler.java
+++ backend/src/main/java/me/grudzien/patryk/oauth2/handlers/CustomOAuth2AuthenticationSuccessHandler.java
@@ -4,7 +4,6 @@ import lombok.extern.log4j.Log4j2;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.core.context.SecurityContextHolder;
-import org.springframework.security.oauth2.core.oidc.user.DefaultOidcUser;
 import org.springframework.security.web.authentication.SimpleUrlAuthenticationSuccessHandler;
 import org.springframework.stereotype.Component;
 
@@ -14,12 +13,17 @@ import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
 import me.grudzien.patryk.PropertiesKeeper;
+import me.grudzien.patryk.oauth2.domain.CustomOAuth2OidcPrincipalUser;
 import me.grudzien.patryk.oauth2.repository.CacheBasedOAuth2AuthorizationRequestRepository;
 import me.grudzien.patryk.utils.jwt.JwtTokenUtil;
+
+import static me.grudzien.patryk.utils.log.LogMarkers.OAUTH2_MARKER;
 
 @Log4j2
 @Component
 public class CustomOAuth2AuthenticationSuccessHandler extends SimpleUrlAuthenticationSuccessHandler {
+
+	public static final String SHORT_LIVED_AUTH_TOKEN_NAME = "shortLivedAuthToken";
 
 	private final CacheBasedOAuth2AuthorizationRequestRepository cacheBasedOAuth2AuthorizationRequestRepository;
 	private final PropertiesKeeper propertiesKeeper;
@@ -35,22 +39,11 @@ public class CustomOAuth2AuthenticationSuccessHandler extends SimpleUrlAuthentic
 
 	@Override
 	protected String determineTargetUrl(final HttpServletRequest request, final HttpServletResponse response) {
-
-		// TODO
-
-		// 1. Get principal user
-		final DefaultOidcUser customOidcPrincipalUser = (DefaultOidcUser) SecurityContextHolder.getContext().getAuthentication().getPrincipal();
-
-		// 2. Create short lived auth token
+		log.info(OAUTH2_MARKER, ">>>> OAUTH2 <<<< determineTargetUrl()");
+		final CustomOAuth2OidcPrincipalUser customOidcPrincipalUser = (CustomOAuth2OidcPrincipalUser) SecurityContextHolder.getContext().getAuthentication().getPrincipal();
 		final String shortLivedAuthToken = JwtTokenUtil.Creator.OAuth2.generateAccessToken(customOidcPrincipalUser);
-
-		// 3. Prepare target url
-		final String targetUrl = "/social-login-success?token=" + shortLivedAuthToken;
-
-		// 4. Delete cache
+		final String targetUrl = propertiesKeeper.oAuth2().SUCCESS_TARGET_URL + "?" + SHORT_LIVED_AUTH_TOKEN_NAME + "=" + shortLivedAuthToken;
 		cacheBasedOAuth2AuthorizationRequestRepository.evictOAuth2AuthorizationRequestCache();
-
-		// 5. return target url + short lived token
 		return targetUrl;
 	}
 }
