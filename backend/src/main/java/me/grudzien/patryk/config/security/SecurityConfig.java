b86a33bc1599c9fd8e118af2fc07a861ebe728a5
patrykgrudzien <patryk.grudzien.91@gmail.com>
2018-09-20 12:13:18 +0200

    SecurityConfig improvements.

--- backend/src/main/java/me/grudzien/patryk/config/security/SecurityConfig.java
+++ backend/src/main/java/me/grudzien/patryk/config/security/SecurityConfig.java
@@ -24,9 +24,6 @@ import org.springframework.security.web.savedrequest.RequestCache;
 
 import com.google.common.base.Preconditions;
 
-import static me.grudzien.patryk.PropertiesKeeper.FrontendRoutes;
-import static me.grudzien.patryk.PropertiesKeeper.StaticResources;
-
 import me.grudzien.patryk.PropertiesKeeper;
 import me.grudzien.patryk.config.filters.JwtAuthorizationTokenFilter;
 import me.grudzien.patryk.config.filters.ServletExceptionHandlerFilter;
@@ -35,8 +32,11 @@ import me.grudzien.patryk.oauth2.handlers.CustomOAuth2AuthenticationSuccessHandl
 import me.grudzien.patryk.oauth2.repository.CacheBasedOAuth2AuthorizationRequestRepository;
 import me.grudzien.patryk.oauth2.service.CustomOAuth2UserService;
 import me.grudzien.patryk.oauth2.service.CustomOidcUserService;
+import me.grudzien.patryk.oauth2.utils.CacheHelper;
 import me.grudzien.patryk.service.security.MyUserDetailsService;
 import me.grudzien.patryk.utils.log.LogMarkers;
+
+import static me.grudzien.patryk.PropertiesKeeper.*;
 
 @Log4j2
 @Configuration
@@ -52,6 +52,7 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
 	private final CustomOAuth2UserService customOAuth2UserService;
 	private final PropertiesKeeper propertiesKeeper;
 	private final CacheManager cacheManager;
+	private final CacheHelper cacheHelper;
 
 	/**
 	 * @Qualifier for {@link org.springframework.security.core.userdetails.UserDetailsService} is used here because there is also
@@ -64,7 +65,7 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
 	                      final CustomOAuth2AuthenticationSuccessHandler customOAuth2AuthenticationSuccessHandler,
 	                      final CustomOAuth2AuthenticationFailureHandler customOAuth2AuthenticationFailureHandler,
 	                      final CustomOidcUserService customOidcUserService, final CustomOAuth2UserService customOAuth2UserService,
-	                      final PropertiesKeeper propertiesKeeper, final CacheManager cacheManager) {
+	                      final PropertiesKeeper propertiesKeeper, final CacheManager cacheManager, final CacheHelper cacheHelper) {
 
 		Preconditions.checkNotNull(userDetailsService, "userDetailsService cannot be null!");
 		Preconditions.checkNotNull(customAuthenticationEntryPoint, "customAuthenticationEntryPoint cannot be null!");
@@ -74,6 +75,7 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
 		Preconditions.checkNotNull(customOAuth2UserService, "customOAuth2UserService cannot be null!");
 		Preconditions.checkNotNull(propertiesKeeper, "propertiesKeeper cannot be null!");
 		Preconditions.checkNotNull(cacheManager, "cacheManager cannot be null!");
+		Preconditions.checkNotNull(cacheHelper, "cacheHelper cannot be null!");
 
 		this.userDetailsService = userDetailsService;
 		this.customAuthenticationEntryPoint = customAuthenticationEntryPoint;
@@ -83,6 +85,7 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
 		this.customOAuth2UserService = customOAuth2UserService;
 		this.propertiesKeeper = propertiesKeeper;
 		this.cacheManager = cacheManager;
+		this.cacheHelper = cacheHelper;
 	}
 
 	@Bean
@@ -138,58 +141,12 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
 		cors(httpSecurity);
 
 		// oauth2 clients
-		oauth2Client(httpSecurity);
+		oauth2Client(httpSecurity, cacheHelper);
 
 		// mvcMatchers
 		authorizeRequests(httpSecurity);
 	}
 
-	@Override
-	public void configure(final WebSecurity web) {
-		// AuthenticationTokenFilter will ignore the below paths
-		web.ignoring()
-		        // allow calls for request methods of "OPTIONS" type -> (CORS purpose) without checking JWT token
-		        // (this helps to avoid duplicate calls before the specific ones)
-		        .mvcMatchers(HttpMethod.OPTIONS, "/**")
-		            .and()
-		   .ignoring()
-		        // /auth
-		        .mvcMatchers(HttpMethod.POST, propertiesKeeper.endpoints().AUTH)
-					.and()
-		    .ignoring()
-		        // /auth/**
-		        .mvcMatchers(HttpMethod.POST, propertiesKeeper.endpoints().AUTH + "/**")
-		            .and()
-			.ignoring()
-		        // /registration/**  (/register-user-account)
-				.mvcMatchers(HttpMethod.POST, propertiesKeeper.endpoints().REGISTRATION + "/**")
-					.and()
-			.ignoring()
-		        // /registration/**
-				.mvcMatchers(HttpMethod.GET, propertiesKeeper.endpoints().REGISTRATION + "/**")
-					.and()
-		   .ignoring()
-		        // /refresh-token
-		        .mvcMatchers(HttpMethod.POST, propertiesKeeper.endpoints().REFRESH_TOKEN)
-		            .and()
-		   .ignoring()
-		        // TODO
-		        .mvcMatchers("/social-login-success**")
-		            .and()
-			.ignoring()
-                .mvcMatchers(HttpMethod.GET, StaticResources.ALL)
-					.and()
-			.ignoring()
-				.mvcMatchers(HttpMethod.GET,
-				             FrontendRoutes.ABOUT_ME,
-				             FrontendRoutes.REGISTRATION_FORM,
-				             FrontendRoutes.REGISTRATION_CONFIRMED,
-				             FrontendRoutes.REGISTRATION_CONFIRMED_WILDCARD,
-				             FrontendRoutes.LOGIN,
-				             FrontendRoutes.MAIN_BOARD,
-				             FrontendRoutes.MAIN_BOARD_WILDCARD,
-				             FrontendRoutes.AUTHENTICATION_REQUIRED);
-	}
 
 	private void sessionCreationPolicy(final HttpSecurity httpSecurity) throws Exception {
 		httpSecurity.sessionManagement()
@@ -240,6 +197,21 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
 		 */
 	}
 
+	private void oauth2Client(final HttpSecurity httpSecurity, final CacheHelper cacheHelper) throws Exception {
+		httpSecurity.oauth2Login()
+		            .loginPage(propertiesKeeper.oAuth2().LOGIN_PAGE)
+		            .authorizationEndpoint()
+		                .authorizationRequestRepository(new CacheBasedOAuth2AuthorizationRequestRepository(cacheManager, cacheHelper))
+		                    .and()
+		            .successHandler(customOAuth2AuthenticationSuccessHandler)
+					.failureHandler(customOAuth2AuthenticationFailureHandler)
+					.userInfoEndpoint()
+		                // OpenID Connect (Google)
+						.oidcUserService(customOidcUserService)
+		                // OAuth2 2.0 (Facebook)
+                        .userService(customOAuth2UserService);
+	}
+
 	private void authorizeRequests(final HttpSecurity httpSecurity) throws Exception {
 		httpSecurity
 				.authorizeRequests()
@@ -256,24 +228,62 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
 				.mvcMatchers(HttpMethod.GET, propertiesKeeper.endpoints().REGISTRATION + "/**").permitAll()
 				// /refresh-token
 				.mvcMatchers(HttpMethod.POST, propertiesKeeper.endpoints().REFRESH_TOKEN).permitAll()
-				// TODO
-				.mvcMatchers("/social-login-success**").permitAll()
+				// //google-login-success**
+				.mvcMatchers(propertiesKeeper.oAuth2().SUCCESS_TARGET_URL + "**").permitAll()
+				// /google-login-failure
+				.mvcMatchers(propertiesKeeper.oAuth2().FAILURE_TARGET_URL + "**").permitAll()
 				// require authentication via JWT
 				.anyRequest().authenticated();
 	}
 
-	private void oauth2Client(final HttpSecurity httpSecurity) throws Exception {
-		httpSecurity.oauth2Login()
-		            .loginPage(propertiesKeeper.oAuth2().LOGIN_PAGE)
-		            .authorizationEndpoint()
-		                .authorizationRequestRepository(new CacheBasedOAuth2AuthorizationRequestRepository(cacheManager))
-		                    .and()
-		            .successHandler(customOAuth2AuthenticationSuccessHandler)
-					.failureHandler(customOAuth2AuthenticationFailureHandler)
-					.userInfoEndpoint()
-		                // OpenID Connect (Google)
-						.oidcUserService(customOidcUserService)
-		                // OAuth2 2.0 (Facebook)
-                        .userService(customOAuth2UserService);
+	@Override
+	public void configure(final WebSecurity web) {
+		// AuthenticationTokenFilter will ignore the below paths
+		web.ignoring()
+		        // allow calls for request methods of "OPTIONS" type -> (CORS purpose) without checking JWT token
+		        // (this helps to avoid duplicate calls before the specific ones)
+		        .mvcMatchers(HttpMethod.OPTIONS, "/**")
+		            .and()
+		   .ignoring()
+		        // /auth
+		        .mvcMatchers(HttpMethod.POST, propertiesKeeper.endpoints().AUTH)
+					.and()
+		    .ignoring()
+		        // /auth/**
+		        .mvcMatchers(HttpMethod.POST, propertiesKeeper.endpoints().AUTH + "/**")
+		            .and()
+			.ignoring()
+		        // /registration/**  (/register-user-account)
+				.mvcMatchers(HttpMethod.POST, propertiesKeeper.endpoints().REGISTRATION + "/**")
+					.and()
+			.ignoring()
+		        // /registration/**
+				.mvcMatchers(HttpMethod.GET, propertiesKeeper.endpoints().REGISTRATION + "/**")
+					.and()
+		   .ignoring()
+		        // /refresh-token
+		        .mvcMatchers(HttpMethod.POST, propertiesKeeper.endpoints().REFRESH_TOKEN)
+		            .and()
+		   .ignoring()
+		        // /google-login-success**
+		        .mvcMatchers(propertiesKeeper.oAuth2().SUCCESS_TARGET_URL + "**")
+		            .and()
+		   .ignoring()
+		        // /google-login-failure
+		        .mvcMatchers(propertiesKeeper.oAuth2().FAILURE_TARGET_URL + "**")
+		            .and()
+			.ignoring()
+                .mvcMatchers(HttpMethod.GET, StaticResources.ALL)
+					.and()
+			.ignoring()
+				.mvcMatchers(HttpMethod.GET,
+				             FrontendRoutes.ABOUT_ME,
+				             FrontendRoutes.REGISTRATION_FORM,
+				             FrontendRoutes.REGISTRATION_CONFIRMED,
+				             FrontendRoutes.REGISTRATION_CONFIRMED_WILDCARD,
+				             FrontendRoutes.LOGIN,
+				             FrontendRoutes.MAIN_BOARD,
+				             FrontendRoutes.MAIN_BOARD_WILDCARD,
+				             FrontendRoutes.AUTHENTICATION_REQUIRED);
 	}
 }
