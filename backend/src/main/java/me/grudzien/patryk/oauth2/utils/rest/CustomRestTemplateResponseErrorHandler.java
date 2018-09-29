b61dea75ea8444a33a5a72a0bd1a1c2b64df235d
PatrykGrudzien <patryk.grudzien.91@gmail.com>
2018-09-29 19:58:54 +0200

    (Feature) Google oauth2 login - own implementation #12.

--- backend/src/main/java/me/grudzien/patryk/oauth2/utils/rest/CustomRestTemplateResponseErrorHandler.java
+++ backend/src/main/java/me/grudzien/patryk/oauth2/utils/rest/CustomRestTemplateResponseErrorHandler.java
@@ -6,19 +6,25 @@ import org.springframework.http.HttpStatus;
 import org.springframework.http.client.ClientHttpResponse;
 import org.springframework.web.client.DefaultResponseErrorHandler;
 
-import com.fasterxml.jackson.databind.ObjectMapper;
+import static me.grudzien.patryk.utils.log.LogMarkers.OAUTH2_MARKER;
 
-import java.io.IOException;
-
-import me.grudzien.patryk.domain.dto.responses.ExceptionResponse;
-
+/**
+ * I'm using that custom RestTemplate inside:
+ * {@link me.grudzien.patryk.oauth2.service.google.impl.GooglePrincipalServiceImpl}
+ * which calls:
+ * {@link me.grudzien.patryk.controller.registration.UserRegistrationController#registerUserAccount(
+ * me.grudzien.patryk.domain.dto.registration.UserRegistrationDto, org.springframework.validation.BindingResult, org.springframework.web.context.request.WebRequest)}
+ * controller.
+ * That controller calls than it's service which can throw some exception and to allow all that flow to proceed,
+ * I override {@link org.springframework.web.client.DefaultResponseErrorHandler} and I don't handle any errors what allows OAuth2 flow to catch failures by:
+ * {@link me.grudzien.patryk.oauth2.handlers.CustomOAuth2AuthenticationFailureHandler}.
+ */
 @Log4j2
 public class CustomRestTemplateResponseErrorHandler extends DefaultResponseErrorHandler {
 
 	@Override
-	public void handleError(final ClientHttpResponse response) throws IOException {
-		final ExceptionResponse exceptionResponse = new ObjectMapper().readValue(response.getBody(), ExceptionResponse.class);
-		log.error(exceptionResponse.getMessage());
+	public void handleError(final ClientHttpResponse response) {
+		log.error(OAUTH2_MARKER, "handlerError() method omitted.");
 	}
 
 	@Override
