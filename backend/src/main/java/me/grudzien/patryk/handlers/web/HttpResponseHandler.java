1f33a81ca8272c94a5cfe83d31f1bb5c59f48c08
PatrykGrudzien <patryk.grudzien.91@gmail.com>
2018-09-30 12:38:46 +0200

    Replaced switch statement with Match() from Vavr library.

--- backend/src/main/java/me/grudzien/patryk/handlers/web/HttpResponseHandler.java
+++ backend/src/main/java/me/grudzien/patryk/handlers/web/HttpResponseHandler.java
@@ -1,6 +1,13 @@
 package me.grudzien.patryk.handlers.web;
 
+import static io.vavr.API.$;
+import static io.vavr.API.Case;
+import static io.vavr.API.Match;
+import static io.vavr.API.run;
+import static io.vavr.Predicates.is;
+
 import io.vavr.Tuple2;
+import io.vavr.control.Try;
 import lombok.extern.log4j.Log4j2;
 
 import org.springframework.beans.factory.annotation.Autowired;
@@ -9,8 +16,6 @@ import org.springframework.stereotype.Component;
 import com.google.common.base.Preconditions;
 
 import javax.servlet.http.HttpServletResponse;
-
-import java.io.IOException;
 
 import static me.grudzien.patryk.domain.enums.AppFLow.ACCOUNT_ALREADY_ENABLED;
 import static me.grudzien.patryk.domain.enums.AppFLow.CONFIRM_REGISTRATION;
@@ -50,64 +55,58 @@ public class HttpResponseHandler {
 
 	@SafeVarargs
 	public final void redirectUserTo(final AppFLow appFLow, final HttpServletResponse response, final Tuple2<String, String>... additionalParameters) {
-		/**
-		 * Creating base app URLs for:
-		 * {@link me.grudzien.patryk.service.registration.impl.UserRegistrationServiceImpl#confirmRegistration(String, javax.servlet.http.HttpServletResponse)}
-		 *
-		 * @return Base app URls which are used to redirect user to specific screens on UI.
-		 */
-		switch (appFLow) {
-			case ACCOUNT_ALREADY_ENABLED:
-				final String userAlreadyEnabledUrl = propertiesKeeper.endpoints().USER_ALREADY_ENABLED;
-				try {
-					response.sendRedirect(contextPathsResolver.determineUrlFor(ACCOUNT_ALREADY_ENABLED) + userAlreadyEnabledUrl);
-					log.info(FLOW_MARKER, ACCOUNT_ALREADY_ENABLED.getSuccessfulRedirectionLogInfoMessage(), userAlreadyEnabledUrl);
-				} catch (final IOException exception) {
-					log.error(EXCEPTION_MARKER, ACCOUNT_ALREADY_ENABLED.getRedirectionExceptionLogErrorMessage(), userAlreadyEnabledUrl);
-					throw new RedirectionException(localeMessagesCreator.buildLocaleMessage("cannot-redirect-to-user-already-enabled-url"));
-				}
-				break;
-			case CONFIRM_REGISTRATION:
-				final String registrationConfirmedUrl = propertiesKeeper.endpoints().REGISTRATION_CONFIRMED;
-				try {
-					response.sendRedirect(contextPathsResolver.determineUrlFor(CONFIRM_REGISTRATION) + registrationConfirmedUrl);
-					log.info(FLOW_MARKER, CONFIRM_REGISTRATION.getSuccessfulRedirectionLogInfoMessage(), registrationConfirmedUrl);
-				} catch (final IOException exception) {
-					log.error(EXCEPTION_MARKER, CONFIRM_REGISTRATION.getRedirectionExceptionLogErrorMessage(), registrationConfirmedUrl);
-					throw new RedirectionException(localeMessagesCreator.buildLocaleMessage("cannot-redirect-to-registration-confirmed-url"));
-				}
-				break;
-			case VERIFICATION_TOKEN_NOT_FOUND:
-				final String confirmedTokenNotFoundUrl = propertiesKeeper.endpoints().CONFIRMATION_TOKEN_NOT_FOUND;
-				try {
-					response.sendRedirect(contextPathsResolver.determineUrlFor(CONFIRM_REGISTRATION) + confirmedTokenNotFoundUrl);
-					log.info(FLOW_MARKER, VERIFICATION_TOKEN_NOT_FOUND.getSuccessfulRedirectionLogInfoMessage(), confirmedTokenNotFoundUrl);
-				} catch (final IOException exception) {
-					log.error(EXCEPTION_MARKER, VERIFICATION_TOKEN_NOT_FOUND.getRedirectionExceptionLogErrorMessage(), confirmedTokenNotFoundUrl);
-					throw new RedirectionException(localeMessagesCreator.buildLocaleMessage("cannot-redirect-to-registration-confirmed-token-not-found-url"));
-				}
-				break;
-			case VERIFICATION_TOKEN_EXPIRED:
-				final String confirmedTokenExpired = propertiesKeeper.endpoints().CONFIRMATION_TOKEN_EXPIRED;
-				try {
-					response.sendRedirect(contextPathsResolver.determineUrlFor(CONFIRM_REGISTRATION) + confirmedTokenExpired);
-					log.info(FLOW_MARKER, VERIFICATION_TOKEN_EXPIRED.getSuccessfulRedirectionLogInfoMessage(), confirmedTokenExpired);
-				} catch (final IOException exception) {
-					log.error(EXCEPTION_MARKER, VERIFICATION_TOKEN_EXPIRED.getRedirectionExceptionLogErrorMessage(), confirmedTokenExpired);
-					throw new RedirectionException(localeMessagesCreator.buildLocaleMessage("cannot-redirect-to-registration-confirmed-token-expired-url"));
-				}
-				break;
-			case GOOGLE_REDIRECTION_SUCCESSFUL:
-				final String googleSuccessTargetUrl = CustomURLBuilder.buildURL(propertiesKeeper.oAuth2().SUCCESS_TARGET_URL, AdditionalParamsDelimiterType.REQUEST_PARAM,
-				                                                                additionalParameters);
-				try {
-					response.sendRedirect(contextPathsResolver.determineUrlFor(GOOGLE_REDIRECTION_SUCCESSFUL) + googleSuccessTargetUrl);
-					log.info(FLOW_MARKER, GOOGLE_REDIRECTION_SUCCESSFUL.getSuccessfulRedirectionLogInfoMessage(), googleSuccessTargetUrl);
-				} catch (final IOException exception) {
-					log.error(EXCEPTION_MARKER, GOOGLE_REDIRECTION_SUCCESSFUL.getRedirectionExceptionLogErrorMessage(), googleSuccessTargetUrl);
-					throw new RedirectionException(localeMessagesCreator.buildLocaleMessage("google-redirection-failure"));
-				}
-				break;
-		}
+		Match(appFLow).of(
+				Case($(is(ACCOUNT_ALREADY_ENABLED)), flow -> run(() -> {
+					final String userAlreadyEnabledUrl = propertiesKeeper.endpoints().USER_ALREADY_ENABLED;
+					Try.run(() -> {
+						response.sendRedirect(contextPathsResolver.determineUrlFor(ACCOUNT_ALREADY_ENABLED) + userAlreadyEnabledUrl);
+						log.info(FLOW_MARKER, ACCOUNT_ALREADY_ENABLED.getSuccessfulRedirectionLogInfoMessage(), userAlreadyEnabledUrl);
+					}).onFailure(throwable -> {
+						log.error(EXCEPTION_MARKER, ACCOUNT_ALREADY_ENABLED.getRedirectionExceptionLogErrorMessage(), userAlreadyEnabledUrl);
+						throw new RedirectionException(localeMessagesCreator.buildLocaleMessage("cannot-redirect-to-user-already-enabled-url"));
+					});
+				})),
+				Case($(is(CONFIRM_REGISTRATION)), flow -> run(() -> {
+					final String registrationConfirmedUrl = propertiesKeeper.endpoints().REGISTRATION_CONFIRMED;
+					Try.run(() -> {
+						response.sendRedirect(contextPathsResolver.determineUrlFor(CONFIRM_REGISTRATION) + registrationConfirmedUrl);
+						log.info(FLOW_MARKER, CONFIRM_REGISTRATION.getSuccessfulRedirectionLogInfoMessage(), registrationConfirmedUrl);
+					}).onFailure(throwable -> {
+						log.error(EXCEPTION_MARKER, CONFIRM_REGISTRATION.getRedirectionExceptionLogErrorMessage(), registrationConfirmedUrl);
+						throw new RedirectionException(localeMessagesCreator.buildLocaleMessage("cannot-redirect-to-registration-confirmed-url"));
+					});
+				})),
+				Case($(is(VERIFICATION_TOKEN_NOT_FOUND)), flow -> run(() -> {
+					final String confirmedTokenNotFoundUrl = propertiesKeeper.endpoints().CONFIRMATION_TOKEN_NOT_FOUND;
+					Try.run(() -> {
+						response.sendRedirect(contextPathsResolver.determineUrlFor(CONFIRM_REGISTRATION) + confirmedTokenNotFoundUrl);
+						log.info(FLOW_MARKER, VERIFICATION_TOKEN_NOT_FOUND.getSuccessfulRedirectionLogInfoMessage(), confirmedTokenNotFoundUrl);
+					}).onFailure(throwable -> {
+						log.error(EXCEPTION_MARKER, VERIFICATION_TOKEN_NOT_FOUND.getRedirectionExceptionLogErrorMessage(), confirmedTokenNotFoundUrl);
+						throw new RedirectionException(localeMessagesCreator.buildLocaleMessage("cannot-redirect-to-registration-confirmed-token-not-found-url"));
+					});
+				})),
+				Case($(is(VERIFICATION_TOKEN_EXPIRED)), flow -> run(() -> {
+					final String confirmedTokenExpired = propertiesKeeper.endpoints().CONFIRMATION_TOKEN_EXPIRED;
+					Try.run(() -> {
+						response.sendRedirect(contextPathsResolver.determineUrlFor(CONFIRM_REGISTRATION) + confirmedTokenExpired);
+						log.info(FLOW_MARKER, VERIFICATION_TOKEN_EXPIRED.getSuccessfulRedirectionLogInfoMessage(), confirmedTokenExpired);
+					}).onFailure(throwable -> {
+						log.error(EXCEPTION_MARKER, VERIFICATION_TOKEN_EXPIRED.getRedirectionExceptionLogErrorMessage(), confirmedTokenExpired);
+						throw new RedirectionException(localeMessagesCreator.buildLocaleMessage("cannot-redirect-to-registration-confirmed-token-expired-url"));
+					});
+				})),
+				Case($(is(GOOGLE_REDIRECTION_SUCCESSFUL)), flow -> run(() -> {
+					final String googleSuccessTargetUrl = CustomURLBuilder.buildURL(propertiesKeeper.oAuth2().SUCCESS_TARGET_URL,
+					                                                                AdditionalParamsDelimiterType.REQUEST_PARAM, additionalParameters);
+					Try.run(() -> {
+						response.sendRedirect(contextPathsResolver.determineUrlFor(GOOGLE_REDIRECTION_SUCCESSFUL) + googleSuccessTargetUrl);
+						log.info(FLOW_MARKER, GOOGLE_REDIRECTION_SUCCESSFUL.getSuccessfulRedirectionLogInfoMessage(), googleSuccessTargetUrl);
+					}).onFailure(throwable -> {
+						log.error(EXCEPTION_MARKER, GOOGLE_REDIRECTION_SUCCESSFUL.getRedirectionExceptionLogErrorMessage(), googleSuccessTargetUrl);
+						throw new RedirectionException(localeMessagesCreator.buildLocaleMessage("google-redirection-failure"));
+					});
+				}))
+		);
 	}
 }
