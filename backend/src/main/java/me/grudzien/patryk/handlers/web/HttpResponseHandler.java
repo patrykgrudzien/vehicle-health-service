626084c48bc9f53b41c246f8854723fcccc77a54
patrykgrudzien <patryk.grudzien.91@gmail.com>
2018-10-05 17:21:55 +0200

    Refactoring.

--- backend/src/main/java/me/grudzien/patryk/handlers/web/HttpResponseHandler.java
+++ backend/src/main/java/me/grudzien/patryk/handlers/web/HttpResponseHandler.java
@@ -1,11 +1,5 @@
 package me.grudzien.patryk.handlers.web;
 
-import static io.vavr.API.$;
-import static io.vavr.API.Case;
-import static io.vavr.API.Match;
-import static io.vavr.API.run;
-import static io.vavr.Predicates.is;
-
 import io.vavr.Tuple2;
 import io.vavr.control.Try;
 import lombok.extern.log4j.Log4j2;
@@ -17,21 +11,26 @@ import com.google.common.base.Preconditions;
 
 import javax.servlet.http.HttpServletResponse;
 
-import static me.grudzien.patryk.domain.enums.AppFLow.ACCOUNT_ALREADY_ENABLED;
-import static me.grudzien.patryk.domain.enums.AppFLow.CONFIRM_REGISTRATION;
-import static me.grudzien.patryk.domain.enums.AppFLow.GOOGLE_REDIRECTION_SUCCESSFUL;
-import static me.grudzien.patryk.domain.enums.AppFLow.VERIFICATION_TOKEN_EXPIRED;
-import static me.grudzien.patryk.domain.enums.AppFLow.VERIFICATION_TOKEN_NOT_FOUND;
-import static me.grudzien.patryk.utils.log.LogMarkers.EXCEPTION_MARKER;
-import static me.grudzien.patryk.utils.log.LogMarkers.FLOW_MARKER;
-import static me.grudzien.patryk.utils.web.CustomURLBuilder.AdditionalParamsDelimiterType;
-
 import me.grudzien.patryk.PropertiesKeeper;
 import me.grudzien.patryk.domain.enums.AppFLow;
 import me.grudzien.patryk.exceptions.RedirectionException;
 import me.grudzien.patryk.utils.i18n.LocaleMessagesCreator;
 import me.grudzien.patryk.utils.web.ContextPathsResolver;
 import me.grudzien.patryk.utils.web.CustomURLBuilder;
+
+import static io.vavr.API.$;
+import static io.vavr.API.Case;
+import static io.vavr.API.Match;
+import static io.vavr.API.run;
+import static io.vavr.Predicates.is;
+import static me.grudzien.patryk.domain.enums.AppFLow.ACCOUNT_ALREADY_ENABLED;
+import static me.grudzien.patryk.domain.enums.AppFLow.CONFIRM_REGISTRATION;
+import static me.grudzien.patryk.domain.enums.AppFLow.USER_LOGGED_IN_USING_GOOGLE;
+import static me.grudzien.patryk.domain.enums.AppFLow.VERIFICATION_TOKEN_EXPIRED;
+import static me.grudzien.patryk.domain.enums.AppFLow.VERIFICATION_TOKEN_NOT_FOUND;
+import static me.grudzien.patryk.utils.log.LogMarkers.EXCEPTION_MARKER;
+import static me.grudzien.patryk.utils.log.LogMarkers.FLOW_MARKER;
+import static me.grudzien.patryk.utils.web.CustomURLBuilder.AdditionalParamsDelimiterType;
 
 @Log4j2
 @Component
@@ -57,53 +56,53 @@ public class HttpResponseHandler {
 	public final void redirectUserTo(final AppFLow appFLow, final HttpServletResponse response, final Tuple2<String, String>... additionalParameters) {
 		Match(appFLow).of(
 				Case($(is(ACCOUNT_ALREADY_ENABLED)), flow -> run(() -> {
-					final String userAlreadyEnabledUrl = propertiesKeeper.endpoints().USER_ALREADY_ENABLED;
+					final String userAlreadyEnabledURL = propertiesKeeper.endpoints().USER_ALREADY_ENABLED;
 					Try.run(() -> {
-						response.sendRedirect(contextPathsResolver.determineUrlFor(ACCOUNT_ALREADY_ENABLED) + userAlreadyEnabledUrl);
-						log.info(FLOW_MARKER, ACCOUNT_ALREADY_ENABLED.getSuccessfulRedirectionLogInfoMessage(), userAlreadyEnabledUrl);
+						response.sendRedirect(contextPathsResolver.determineUrlFor(ACCOUNT_ALREADY_ENABLED) + userAlreadyEnabledURL);
+						log.info(FLOW_MARKER, ACCOUNT_ALREADY_ENABLED.getSuccessfulRedirectionLogInfoMessage(), userAlreadyEnabledURL);
 					}).onFailure(throwable -> {
-						log.error(EXCEPTION_MARKER, ACCOUNT_ALREADY_ENABLED.getRedirectionExceptionLogErrorMessage(), userAlreadyEnabledUrl);
+						log.error(EXCEPTION_MARKER, ACCOUNT_ALREADY_ENABLED.getRedirectionExceptionLogErrorMessage(), userAlreadyEnabledURL);
 						throw new RedirectionException(localeMessagesCreator.buildLocaleMessage("cannot-redirect-to-user-already-enabled-url"));
 					});
 				})),
 				Case($(is(CONFIRM_REGISTRATION)), flow -> run(() -> {
-					final String registrationConfirmedUrl = propertiesKeeper.endpoints().REGISTRATION_CONFIRMED;
+					final String registrationConfirmedURL = propertiesKeeper.endpoints().REGISTRATION_CONFIRMED;
 					Try.run(() -> {
-						response.sendRedirect(contextPathsResolver.determineUrlFor(CONFIRM_REGISTRATION) + registrationConfirmedUrl);
-						log.info(FLOW_MARKER, CONFIRM_REGISTRATION.getSuccessfulRedirectionLogInfoMessage(), registrationConfirmedUrl);
+						response.sendRedirect(contextPathsResolver.determineUrlFor(CONFIRM_REGISTRATION) + registrationConfirmedURL);
+						log.info(FLOW_MARKER, CONFIRM_REGISTRATION.getSuccessfulRedirectionLogInfoMessage(), registrationConfirmedURL);
 					}).onFailure(throwable -> {
-						log.error(EXCEPTION_MARKER, CONFIRM_REGISTRATION.getRedirectionExceptionLogErrorMessage(), registrationConfirmedUrl);
+						log.error(EXCEPTION_MARKER, CONFIRM_REGISTRATION.getRedirectionExceptionLogErrorMessage(), registrationConfirmedURL);
 						throw new RedirectionException(localeMessagesCreator.buildLocaleMessage("cannot-redirect-to-registration-confirmed-url"));
 					});
 				})),
 				Case($(is(VERIFICATION_TOKEN_NOT_FOUND)), flow -> run(() -> {
-					final String confirmedTokenNotFoundUrl = propertiesKeeper.endpoints().CONFIRMATION_TOKEN_NOT_FOUND;
+					final String confirmedTokenNotFoundURL = propertiesKeeper.endpoints().CONFIRMATION_TOKEN_NOT_FOUND;
 					Try.run(() -> {
-						response.sendRedirect(contextPathsResolver.determineUrlFor(CONFIRM_REGISTRATION) + confirmedTokenNotFoundUrl);
-						log.info(FLOW_MARKER, VERIFICATION_TOKEN_NOT_FOUND.getSuccessfulRedirectionLogInfoMessage(), confirmedTokenNotFoundUrl);
+						response.sendRedirect(contextPathsResolver.determineUrlFor(CONFIRM_REGISTRATION) + confirmedTokenNotFoundURL);
+						log.info(FLOW_MARKER, VERIFICATION_TOKEN_NOT_FOUND.getSuccessfulRedirectionLogInfoMessage(), confirmedTokenNotFoundURL);
 					}).onFailure(throwable -> {
-						log.error(EXCEPTION_MARKER, VERIFICATION_TOKEN_NOT_FOUND.getRedirectionExceptionLogErrorMessage(), confirmedTokenNotFoundUrl);
+						log.error(EXCEPTION_MARKER, VERIFICATION_TOKEN_NOT_FOUND.getRedirectionExceptionLogErrorMessage(), confirmedTokenNotFoundURL);
 						throw new RedirectionException(localeMessagesCreator.buildLocaleMessage("cannot-redirect-to-registration-confirmed-token-not-found-url"));
 					});
 				})),
 				Case($(is(VERIFICATION_TOKEN_EXPIRED)), flow -> run(() -> {
-					final String confirmedTokenExpired = propertiesKeeper.endpoints().CONFIRMATION_TOKEN_EXPIRED;
+					final String confirmedTokenExpiredURL = propertiesKeeper.endpoints().CONFIRMATION_TOKEN_EXPIRED;
 					Try.run(() -> {
-						response.sendRedirect(contextPathsResolver.determineUrlFor(CONFIRM_REGISTRATION) + confirmedTokenExpired);
-						log.info(FLOW_MARKER, VERIFICATION_TOKEN_EXPIRED.getSuccessfulRedirectionLogInfoMessage(), confirmedTokenExpired);
+						response.sendRedirect(contextPathsResolver.determineUrlFor(CONFIRM_REGISTRATION) + confirmedTokenExpiredURL);
+						log.info(FLOW_MARKER, VERIFICATION_TOKEN_EXPIRED.getSuccessfulRedirectionLogInfoMessage(), confirmedTokenExpiredURL);
 					}).onFailure(throwable -> {
-						log.error(EXCEPTION_MARKER, VERIFICATION_TOKEN_EXPIRED.getRedirectionExceptionLogErrorMessage(), confirmedTokenExpired);
+						log.error(EXCEPTION_MARKER, VERIFICATION_TOKEN_EXPIRED.getRedirectionExceptionLogErrorMessage(), confirmedTokenExpiredURL);
 						throw new RedirectionException(localeMessagesCreator.buildLocaleMessage("cannot-redirect-to-registration-confirmed-token-expired-url"));
 					});
 				})),
-				Case($(is(GOOGLE_REDIRECTION_SUCCESSFUL)), flow -> run(() -> {
-					final String googleSuccessTargetUrl = CustomURLBuilder.buildURL(propertiesKeeper.oAuth2().SUCCESS_TARGET_URL,
-					                                                                AdditionalParamsDelimiterType.REQUEST_PARAM, additionalParameters);
+				Case($(is(USER_LOGGED_IN_USING_GOOGLE)), flow -> run(() -> {
+					final String userLoggedInUsingGoogleURL = CustomURLBuilder.buildURL(propertiesKeeper.oAuth2().USER_LOGGED_IN_USING_GOOGLE,
+					                                                                    AdditionalParamsDelimiterType.REQUEST_PARAM, additionalParameters);
 					Try.run(() -> {
-						response.sendRedirect(contextPathsResolver.determineUrlFor(GOOGLE_REDIRECTION_SUCCESSFUL) + googleSuccessTargetUrl);
-						log.info(FLOW_MARKER, GOOGLE_REDIRECTION_SUCCESSFUL.getSuccessfulRedirectionLogInfoMessage(), googleSuccessTargetUrl);
+						response.sendRedirect(contextPathsResolver.determineUrlFor(USER_LOGGED_IN_USING_GOOGLE) + userLoggedInUsingGoogleURL);
+						log.info(FLOW_MARKER, USER_LOGGED_IN_USING_GOOGLE.getSuccessfulRedirectionLogInfoMessage(), userLoggedInUsingGoogleURL);
 					}).onFailure(throwable -> {
-						log.error(EXCEPTION_MARKER, GOOGLE_REDIRECTION_SUCCESSFUL.getRedirectionExceptionLogErrorMessage(), googleSuccessTargetUrl);
+						log.error(EXCEPTION_MARKER, USER_LOGGED_IN_USING_GOOGLE.getRedirectionExceptionLogErrorMessage(), userLoggedInUsingGoogleURL);
 						throw new RedirectionException(localeMessagesCreator.buildLocaleMessage("google-redirection-failure"));
 					});
 				}))
