a66e9fa663cb4fe99194917bc00856e083f875bb
PatrykGrudzien <patryk.grudzien.91@gmail.com>
2018-11-05 00:14:53 +0100

    Failure cases.

--- backend/src/main/java/me/grudzien/patryk/oauth2/authentication/FailedAuthenticationCases.java
+++ backend/src/main/java/me/grudzien/patryk/oauth2/authentication/FailedAuthenticationCases.java
@@ -12,6 +12,7 @@ import org.springframework.security.core.userdetails.UsernameNotFoundException;
 import me.grudzien.patryk.exceptions.login.BadCredentialsAuthenticationException;
 import me.grudzien.patryk.exceptions.login.UserDisabledAuthenticationException;
 import me.grudzien.patryk.oauth2.exceptions.JwtTokenNotFoundException;
+import me.grudzien.patryk.oauth2.exceptions.RegistrationProviderMismatchException;
 import me.grudzien.patryk.utils.i18n.LocaleMessagesCreator;
 
 import static io.vavr.API.$;
@@ -25,19 +26,59 @@ import static me.grudzien.patryk.utils.log.LogMarkers.EXCEPTION_MARKER;
 @Log4j2
 public abstract class FailedAuthenticationCases {
 
-	public static Match.Case<DisabledException, Void> UserDisabledExceptionCase(final String email, final LocaleMessagesCreator localeMessagesCreator) {
-		return Case($(instanceOf(DisabledException.class)), exception -> run(() -> {
-			/**
-			 * Exception thrown below is determined in:
-			 * {@link org.springframework.security.authentication.dao.AbstractUserDetailsAuthenticationProvider#preAuthenticationChecks}
-			 * which points to:
-			 * {@link org.springframework.security.authentication.dao.AbstractUserDetailsAuthenticationProvider.DefaultPreAuthenticationChecks#check(
-			 * org.springframework.security.core.userdetails.UserDetails)}
-			 */
-			log.error(EXCEPTION_MARKER, "User with {} is disabled! Error message -> {}", email, exception.getMessage());
-			throw new UserDisabledAuthenticationException(localeMessagesCreator.buildLocaleMessage("user-disabled-exception"));
-		}));
-	}
+    public static Match.Case<UsernameNotFoundException, Void> UsernameNotFoundExceptionCase(final String email, final LocaleMessagesCreator localeMessagesCreator) {
+        return Case($(instanceOf(UsernameNotFoundException.class)), exception -> run(() -> {
+            throw new UsernameNotFoundException(localeMessagesCreator.buildLocaleMessageWithParam("user-not-found-by-email", email));
+        }));
+    }
+
+    public static Match.Case<LockedException, Void> UserAccountIsLockedExceptionCase(final LocaleMessagesCreator localeMessagesCreator) {
+        return Case($(instanceOf(LockedException.class)), exception -> run(() -> {
+            log.error(EXCEPTION_MARKER, "User account is locked! Details -> {}", exception.getMessage());
+            throw new LockedException(localeMessagesCreator.buildLocaleMessage("user-account-is-locked"));
+        }));
+    }
+
+    public static Match.Case<DisabledException, Void> UserIsDisabledExceptionCase(final String email, final LocaleMessagesCreator localeMessagesCreator) {
+        return Case($(instanceOf(DisabledException.class)), exception -> run(() -> {
+            /**
+             * Exception thrown below is determined in:
+             * {@link org.springframework.security.authentication.dao.AbstractUserDetailsAuthenticationProvider#preAuthenticationChecks}
+             * which points to:
+             * {@link org.springframework.security.authentication.dao.AbstractUserDetailsAuthenticationProvider.DefaultPreAuthenticationChecks#check(
+             * org.springframework.security.core.userdetails.UserDetails)}
+             */
+            log.error(EXCEPTION_MARKER, "User with {} is disabled! Error message -> {}", email, exception.getMessage());
+            throw new UserDisabledAuthenticationException(localeMessagesCreator.buildLocaleMessage("user-disabled-exception"));
+        }));
+    }
+
+    public static Match.Case<AccountExpiredException, Void> UserAccountIsExpiredExceptionCase(final LocaleMessagesCreator localeMessagesCreator) {
+        return Case($(instanceOf(AccountExpiredException.class)), exception -> run(() -> {
+            log.error(EXCEPTION_MARKER, "User account is expired! Details -> {}", exception.getMessage());
+            throw new AccountExpiredException(localeMessagesCreator.buildLocaleMessage("user-account-is-expired"));
+        }));
+    }
+
+    public static Match.Case<CredentialsExpiredException, Void> CredentialsHaveExpiredExceptionCase(final LocaleMessagesCreator localeMessagesCreator) {
+        return Case($(instanceOf(CredentialsExpiredException.class)), exception -> run(() -> {
+            log.error(EXCEPTION_MARKER, "User account credentials have expired! Details -> {}", exception.getMessage());
+            throw new CredentialsExpiredException(localeMessagesCreator.buildLocaleMessage("user-account-credentials-have-expired"));
+        }));
+    }
+
+    public static Match.Case<JwtTokenNotFoundException, Void> JwtTokenNotFoundExceptionCase(final LocaleMessagesCreator localeMessagesCreator) {
+        return Case($(instanceOf(JwtTokenNotFoundException.class)), exception -> run(() -> {
+            log.error(EXCEPTION_MARKER, "Authentication failed! No credentials (JWT Token) provided during SSO login! Details -> {}", exception.getMessage());
+            throw new JwtTokenNotFoundException(localeMessagesCreator.buildLocaleMessage("jwt-token-not-found-exception"));
+        }));
+    }
+
+    public static Match.Case<RegistrationProviderMismatchException, Void> RegistrationProviderMismatchExceptionCase(final LocaleMessagesCreator localeMessagesCreator) {
+        return Case($(instanceOf(RegistrationProviderMismatchException.class)), exception -> run(() -> {
+            throw new RegistrationProviderMismatchException(localeMessagesCreator.buildLocaleMessage("registration-provider-mismatch-exception"));
+        }));
+    }
 
 	public static Match.Case<BadCredentialsException, Void> BadCredentialsExceptionCase(final LocaleMessagesCreator localeMessagesCreator) {
 		return Case($(instanceOf(BadCredentialsException.class)), exception -> run(() -> {
@@ -49,40 +90,6 @@ public abstract class FailedAuthenticationCases {
 			 * {@link me.grudzien.patryk.service.security.MyUserDetailsService}
 			 */
 			throw new BadCredentialsAuthenticationException(localeMessagesCreator.buildLocaleMessage("bad-credentials-exception"));
-		}));
-	}
-
-	public static Match.Case<LockedException, Void> UserAccountIsLockedExceptionCase(final LocaleMessagesCreator localeMessagesCreator) {
-		return Case($(instanceOf(LockedException.class)), exception -> run(() -> {
-			log.error(EXCEPTION_MARKER, "User account is locked! Details -> {}", exception.getMessage());
-			throw new LockedException(localeMessagesCreator.buildLocaleMessage("user-account-is-locked"));
-		}));
-	}
-
-	public static Match.Case<AccountExpiredException, Void> UserAccountIsExpiredExceptionCase(final LocaleMessagesCreator localeMessagesCreator) {
-		return Case($(instanceOf(AccountExpiredException.class)), exception -> run(() -> {
-			log.error(EXCEPTION_MARKER, "User account is expired! Details -> {}", exception.getMessage());
-			throw new AccountExpiredException(localeMessagesCreator.buildLocaleMessage("user-account-is-expired"));
-		}));
-	}
-
-	public static Match.Case<CredentialsExpiredException, Void> CredentialsExpiredExceptionCase(final LocaleMessagesCreator localeMessagesCreator) {
-		return Case($(instanceOf(CredentialsExpiredException.class)), exception -> run(() -> {
-			log.error(EXCEPTION_MARKER, "User account credentials have expired! Details -> {}", exception.getMessage());
-			throw new CredentialsExpiredException(localeMessagesCreator.buildLocaleMessage("user-account-credentials-have-expired"));
-		}));
-	}
-
-	public static Match.Case<JwtTokenNotFoundException, Void> JwtTokenNotFoundExceptionCase(final LocaleMessagesCreator localeMessagesCreator) {
-		return Case($(instanceOf(JwtTokenNotFoundException.class)), exception -> run(() -> {
-			log.error(EXCEPTION_MARKER, "Authentication failed! No credentials (JWT Token) provided during SSO login! Details -> {}", exception.getMessage());
-			throw new JwtTokenNotFoundException(localeMessagesCreator.buildLocaleMessage("jwt-token-not-found-exception"));
-		}));
-	}
-
-	public static Match.Case<UsernameNotFoundException, Void> UsernameNotFoundExceptionCase(final String email, final LocaleMessagesCreator localeMessagesCreator) {
-		return Case($(instanceOf(UsernameNotFoundException.class)), exception -> run(() -> {
-			throw new UsernameNotFoundException(localeMessagesCreator.buildLocaleMessageWithParam("user-not-found-by-email", email));
 		}));
 	}
 }
