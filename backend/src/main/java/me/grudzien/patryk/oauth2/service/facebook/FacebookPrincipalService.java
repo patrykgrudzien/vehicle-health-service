fcc4402051fe3c1c34b39972b12a109ecf6ccc78
PatrykGrudzien <patryk.grudzien.91@gmail.com>
2018-11-05 00:14:23 +0100

    Refactoring.

--- backend/src/main/java/me/grudzien/patryk/oauth2/service/facebook/FacebookPrincipalService.java
+++ backend/src/main/java/me/grudzien/patryk/oauth2/service/facebook/FacebookPrincipalService.java
@@ -7,5 +7,5 @@ import me.grudzien.patryk.oauth2.utils.OAuth2FlowDelegator;
 
 public interface FacebookPrincipalService {
 
-	CustomOAuth2OidcPrincipalUser finishOAuthFlowAndPreparePrincipal(final OAuth2FlowDelegator.OAuth2Flow oAuth2Flow, final OAuth2User oAuth2User);
+	CustomOAuth2OidcPrincipalUser prepareFacebookPrincipal(final OAuth2FlowDelegator.OAuth2Flow oAuth2Flow, final OAuth2User oAuth2User);
 }
