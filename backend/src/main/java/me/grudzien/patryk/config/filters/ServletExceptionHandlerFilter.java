7f64e25c1875e03691a96abdb05cbc68b6bbdf59
patrykgrudzien <patryk.grudzien.91@gmail.com>
2018-11-15 11:06:45 +0100

    Added comments to custom filters taken from Spring Security Architecture documentation.

--- backend/src/main/java/me/grudzien/patryk/config/filters/ServletExceptionHandlerFilter.java
+++ backend/src/main/java/me/grudzien/patryk/config/filters/ServletExceptionHandlerFilter.java
@@ -28,9 +28,26 @@ import static me.grudzien.patryk.util.log.LogMarkers.EXCEPTION_MARKER;
 import static me.grudzien.patryk.util.log.LogMarkers.FLOW_MARKER;
 
 /**
- * Filters CANNOT be managed by Spring explicitly !!!
- * It's NOT ALLOWED to mark them using (@Component) annotation !!!
- * In other case Spring Security does not work properly and does not ignore specified paths.
+ * <hr><br>
+ * The Spring Security {@link org.springframework.security.web.FilterChainProxy} dispatches requests to the first chain that matches.
+ * A vanilla Spring Boot application with no custom security configuration has a several (call it n) filter chains, where usually n=6.
+ * The first (n-1) chains are there just to ignore static resource patterns, like {@linkplain /css/**} and {@linkplain /images/**}, and
+ * the error view {@linkplain /error} (the paths can be controlled by the user with {@linkplain security.ignored} from the
+ * {@linkplain org.springframework.boot.autoconfigure.security.SecurityProperties} configuration bean).
+ * The last chain matches to catch all path {@linkplain /**} and is more active, containing logic for authentication, authorization,
+ * exception handling, session handling, header writing, etc. There are a total of 11 filters in this chain by default, but normally
+ * it is not necessary for users to concern themselves with which filters are used and when.
+ *
+ * <br><br>
+ *
+ * <h2>Note:</h2>
+ * <i>The fact that all filters internal to <b>Spring Security</b> are unknown to the container is important, especially in a Spring Boot
+ * application, where all {@linkplain @Beans} of type {@linkplain Filter} are registered automatically with the container by default.
+ * So if you want to add a custom filter to the security chain, you need to either <b>NOT</b> make it a {@linkplain @Bean} or wrap it in
+ * a {@link org.springframework.boot.web.servlet.FilterRegistrationBean} that explicitly disables the container registration.</u>
+ *
+ * <br><br><hr><br>
+ *
  * Another filter:
  * {@link me.grudzien.patryk.config.filters.GenericJwtTokenFilter}
  *
