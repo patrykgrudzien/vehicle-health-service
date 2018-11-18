319c9c21fc307517f09ad055e5e4096e5d280910
PatrykGrudzien <patryk.grudzien.91@gmail.com>
2018-11-18 12:53:59 +0100

    Added vuetify-fake-components-for-webstorm.js file to turn off WebStorm's warnings about undefined html tags.

---
+++ frontend/vuetify-fake-components-for-webstorm.js
@@ -0,0 +1,5 @@
+// this file is a fake file only created for purpose of Webstorm to autocomplete Vuetify components
+import Vue from 'vue';
+
+Vue.component('v-alert', {});
+Vue.component('v-app', {});
