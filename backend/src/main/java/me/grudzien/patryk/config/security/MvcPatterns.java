223bf30c6aa77c056dfd7bd05cbe5e8cd93d451c
PatrykGrudzien <patryk.grudzien.91@gmail.com>
2018-11-23 10:41:59 +0100

    Added GoogleOAuth2ControllerIntegrationTest and adjusted security configuration to it.

--- backend/src/main/java/me/grudzien/patryk/config/security/MvcPatterns.java
+++ backend/src/main/java/me/grudzien/patryk/config/security/MvcPatterns.java
@@ -39,9 +39,19 @@ final class MvcPatterns {
         return propertiesKeeper.endpoints().API_CONTEXT_PATH + propertiesKeeper.oAuth2().USER_NOT_FOUND + "**";
     }
 
+    static String userAccountIsLockedWildcard(final PropertiesKeeper propertiesKeeper) {
+        // /api/user-account-is-locked**
+        return propertiesKeeper.endpoints().API_CONTEXT_PATH + propertiesKeeper.oAuth2().USER_ACCOUNT_IS_LOCKED + "**";
+    }
+
     static String userIsDisabledWildcard(final PropertiesKeeper propertiesKeeper) {
         // /api/user-is-disabled**
         return propertiesKeeper.endpoints().API_CONTEXT_PATH + propertiesKeeper.oAuth2().USER_IS_DISABLED + "**";
+    }
+
+    static String userAccountIsExpiredWildcard(final PropertiesKeeper propertiesKeeper) {
+        // /api/user-account-is-expired**
+        return propertiesKeeper.endpoints().API_CONTEXT_PATH + propertiesKeeper.oAuth2().USER_ACCOUNT_IS_EXPIRED + "**";
     }
 
     static String userRegisteredUsingGoogleWildcard(final PropertiesKeeper propertiesKeeper) {
@@ -52,6 +62,31 @@ final class MvcPatterns {
     static String userAccountAlreadyExistsWildcard(final PropertiesKeeper propertiesKeeper) {
         // /api/user-account-already-exists**
         return propertiesKeeper.endpoints().API_CONTEXT_PATH + propertiesKeeper.oAuth2().USER_ACCOUNT_ALREADY_EXISTS + "**";
+    }
+
+    static String credentialsHaveExpiredWildcard(final PropertiesKeeper propertiesKeeper) {
+        // /api/credentials-have-expired**
+        return propertiesKeeper.endpoints().API_CONTEXT_PATH + propertiesKeeper.oAuth2().CREDENTIALS_HAVE_EXPIRED + "**";
+    }
+
+    static String jwtTokenNotFoundWildcard(final PropertiesKeeper propertiesKeeper) {
+        // /api/jwt-token-not-found**
+        return propertiesKeeper.endpoints().API_CONTEXT_PATH + propertiesKeeper.oAuth2().JWT_TOKEN_NOT_FOUND + "**";
+    }
+
+    static String registrationProviderMismatchWildcard(final PropertiesKeeper propertiesKeeper) {
+        // /api/registration-provider-mismatch**
+        return propertiesKeeper.endpoints().API_CONTEXT_PATH + propertiesKeeper.oAuth2().REGISTRATION_PROVIDER_MISMATCH + "**";
+    }
+
+    static String badCredentialsWildcard(final PropertiesKeeper propertiesKeeper) {
+        // /api/bad-credentials**
+        return propertiesKeeper.endpoints().API_CONTEXT_PATH + propertiesKeeper.oAuth2().BAD_CREDENTIALS + "**";
+    }
+
+    static String exchangeShortLivedTokenWildcard(final PropertiesKeeper propertiesKeeper) {
+        // /api/exchange-short-lived-token**
+        return propertiesKeeper.endpoints().API_CONTEXT_PATH + propertiesKeeper.oAuth2().EXCHANGE_SHORT_LIVED_TOKEN + "**";
     }
 
     static String failureTargetUrlWildcard(final PropertiesKeeper propertiesKeeper) {
