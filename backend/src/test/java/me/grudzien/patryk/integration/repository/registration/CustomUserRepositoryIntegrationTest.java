32c10ee4d81b7a5c65e3c0aa8d74ec4585013e81
patrykgrudzien <patryk.grudzien.91@gmail.com>
2018-11-26 11:42:36 +0100

    Test improvement.

--- backend/src/test/java/me/grudzien/patryk/integration/repository/registration/CustomUserRepositoryIntegrationTest.java
+++ backend/src/test/java/me/grudzien/patryk/integration/repository/registration/CustomUserRepositoryIntegrationTest.java
@@ -18,8 +18,10 @@ import me.grudzien.patryk.domain.enums.registration.RegistrationProvider;
 import me.grudzien.patryk.domain.enums.registration.RoleName;
 import me.grudzien.patryk.repository.registration.CustomUserRepository;
 
+import static org.assertj.core.api.Assertions.assertThat;
+
 @DataJpaTest
-class CustomUserRepositoryTest {
+class CustomUserRepositoryIntegrationTest {
 
     @Autowired
     private TestEntityManager testEntityManager;
@@ -36,24 +38,32 @@ class CustomUserRepositoryTest {
                                               .lastName("LastName")
                                               .email("test@email.com")
                                               .password("password")
-                                              .createdDate(new Date())
+                                              .createdDate(new Date(123456789L))
                                               .hasFakeEmail(true)
                                               .registrationProvider(RegistrationProvider.CUSTOM)
                                               .roles(Sets.newHashSet(new Role(RoleName.ROLE_USER)))
                                               .isEnabled(false)
                                               .build();
-        testEntityManager.persist(testUser);
-        testEntityManager.flush();
+	    final CustomUser savedUser = testEntityManager.persistFlushFind(testUser);
 
         // when
         final CustomUser foundUser = customUserRepository.findByEmail(testUser.getEmail());
 
         // then
         Assertions.assertAll(
+                () -> assertThat(foundUser.getFirstName()).isEqualTo(savedUser.getFirstName()),
+                () -> assertThat(foundUser.getLastName()).isEqualTo(savedUser.getLastName()),
+                () -> assertThat(foundUser.getEmail()).isEqualTo(savedUser.getEmail()),
+                () -> assertThat(foundUser.getPassword()).isEqualTo(savedUser.getPassword()),
                 () -> Assertions.assertEquals("FirstName", foundUser.getFirstName()),
                 () -> Assertions.assertEquals("LastName", foundUser.getLastName()),
                 () -> Assertions.assertEquals("test@email.com", foundUser.getEmail()),
-                () -> Assertions.assertEquals("password", foundUser.getPassword())
+                () -> Assertions.assertEquals("password", foundUser.getPassword()),
+                () -> Assertions.assertEquals(new Date(123456789L), foundUser.getCreatedDate()),
+                () -> Assertions.assertTrue(foundUser.isHasFakeEmail()),
+                () -> Assertions.assertEquals(RegistrationProvider.CUSTOM, foundUser.getRegistrationProvider()),
+                () -> assertThat(foundUser.getRoles()).hasSize(1),
+                () -> Assertions.assertFalse(foundUser.isEnabled())
         );
     }
 }
