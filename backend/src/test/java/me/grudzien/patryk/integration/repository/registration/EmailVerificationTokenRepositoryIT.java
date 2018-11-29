3f92a321c5dd38e850802cbf224985901de6945d
PatrykGrudzien <patryk.grudzien.91@gmail.com>
2018-11-29 21:46:43 +0100

    Improvements to EmailVerificationTokenRepositoryIT.

--- backend/src/test/java/me/grudzien/patryk/integration/repository/registration/EmailVerificationTokenRepositoryIT.java
+++ backend/src/test/java/me/grudzien/patryk/integration/repository/registration/EmailVerificationTokenRepositoryIT.java
@@ -5,13 +5,18 @@ import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
 import org.springframework.boot.test.autoconfigure.orm.jpa.TestEntityManager;
 
 import org.apache.commons.lang3.RandomStringUtils;
+import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
 import java.util.Date;
+import java.util.Optional;
 
 import me.grudzien.patryk.domain.entity.registration.CustomUser;
 import me.grudzien.patryk.domain.entity.registration.EmailVerificationToken;
+import me.grudzien.patryk.repository.registration.CustomUserRepository;
 import me.grudzien.patryk.repository.registration.EmailVerificationTokenRepository;
 
 import static org.assertj.core.api.Assertions.assertThat;
@@ -25,16 +30,30 @@ class EmailVerificationTokenRepositoryIT {
     @Autowired
     private EmailVerificationTokenRepository emailVerificationTokenRepository;
 
+    @Autowired
+    private CustomUserRepository customUserRepository;
+
+    private EmailVerificationToken givenEmailVerificationToken;
+    private EmailVerificationToken savedToken;
+
+    @BeforeEach
+    void setUp() {
+        givenEmailVerificationToken = prepareTestEmailVerificationToken();
+        savedToken = emailVerificationTokenRepository.save(givenEmailVerificationToken);
+        testEntityManager.flush();
+    }
+
+    @AfterEach
+    void tearDown() {
+        emailVerificationTokenRepository.deleteAll();
+        customUserRepository.deleteAll();
+    }
+
     @SuppressWarnings("Duplicates")
     @Test
     void findByToken() {
-        // given
-        final EmailVerificationToken emailVerificationToken = prepareTestEmailVerificationToken();
-        final EmailVerificationToken savedToken = emailVerificationTokenRepository.save(emailVerificationToken);
-        testEntityManager.flush();
-
         // when
-        final EmailVerificationToken foundToken = emailVerificationTokenRepository.findByToken(emailVerificationToken.getToken());
+        final EmailVerificationToken foundToken = emailVerificationTokenRepository.findByToken(givenEmailVerificationToken.getToken());
 
         // then
         Assertions.assertAll(
@@ -43,20 +62,16 @@ class EmailVerificationTokenRepositoryIT {
                 () -> Assertions.assertNotNull(foundToken.getCustomUser()),
                 () -> assertThat(foundToken.getCustomUser()).isEqualTo(savedToken.getCustomUser()),
                 () -> assertThat(foundToken.getExpiryDate()).isEqualTo(savedToken.getExpiryDate()),
-                () -> Assertions.assertEquals(new Date(12345L), foundToken.getExpiryDate())
+                () -> Assertions.assertEquals(new Date(12345L), foundToken.getExpiryDate()),
+                () -> assertThat(customUserRepository.findById(foundToken.getCustomUser().getId())).isNotNull()
         );
     }
 
     @SuppressWarnings("Duplicates")
     @Test
     void findByCustomUser() {
-        // given
-        final EmailVerificationToken emailVerificationToken = prepareTestEmailVerificationToken();
-        final EmailVerificationToken savedToken = emailVerificationTokenRepository.save(emailVerificationToken);
-        testEntityManager.flush();
-
         // when
-        final EmailVerificationToken foundToken = emailVerificationTokenRepository.findByCustomUser(emailVerificationToken.getCustomUser());
+        final EmailVerificationToken foundToken = emailVerificationTokenRepository.findByCustomUser(givenEmailVerificationToken.getCustomUser());
 
         // then
         Assertions.assertAll(
@@ -65,7 +80,21 @@ class EmailVerificationTokenRepositoryIT {
                 () -> Assertions.assertNotNull(foundToken.getCustomUser()),
                 () -> assertThat(foundToken.getCustomUser()).isEqualTo(savedToken.getCustomUser()),
                 () -> assertThat(foundToken.getExpiryDate()).isEqualTo(savedToken.getExpiryDate()),
-                () -> Assertions.assertEquals(new Date(12345L), foundToken.getExpiryDate())
+                () -> Assertions.assertEquals(new Date(12345L), foundToken.getExpiryDate()),
+                () -> assertThat(customUserRepository.findById(foundToken.getCustomUser().getId())).isNotNull()
+        );
+    }
+
+    @Test
+    @DisplayName("Testing cascading on token deletion (if user is still present).")
+    void deleteToken_userStillPresent() {
+        // when
+        emailVerificationTokenRepository.deleteById(givenEmailVerificationToken.getId());
+
+        // then
+        Assertions.assertAll(
+                () -> assertThat(emailVerificationTokenRepository.findById(givenEmailVerificationToken.getId())).isEqualTo(Optional.empty()),
+                () -> assertThat(customUserRepository.findById(givenEmailVerificationToken.getCustomUser().getId())).isNotNull()
         );
     }
 
