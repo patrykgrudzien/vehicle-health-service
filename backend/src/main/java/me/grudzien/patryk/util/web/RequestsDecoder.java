b51c8e34ca7c190e4640ae7a282ecffe12a248be
PatrykGrudzien <patryk.grudzien.91@gmail.com>
2018-11-30 12:04:23 +0100

    Enhanced RequestDecoder and added integration test to it with custom @ContextConfiguration.

--- backend/src/main/java/me/grudzien/patryk/util/web/RequestsDecoder.java
+++ backend/src/main/java/me/grudzien/patryk/util/web/RequestsDecoder.java
@@ -1,34 +1,58 @@
 package me.grudzien.patryk.util.web;
 
+import lombok.extern.log4j.Log4j2;
+
 import org.springframework.beans.factory.config.BeanDefinition;
 import org.springframework.context.annotation.Scope;
 import org.springframework.stereotype.Component;
 import org.springframework.util.StringUtils;
 
 import java.util.Base64;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
 
+@Log4j2
 @Component
 @Scope(BeanDefinition.SCOPE_PROTOTYPE)
 public class RequestsDecoder {
 
-	public String decodeStringParam(final String arg) {
+    private static final String IS_BASE_64_ENCODED_PATTERN = "^([A-Za-z0-9+/]{4})*([A-Za-z0-9+/]{3}=|[A-Za-z0-9+/]{2}==)?$";
 
+	public String decodeStringParam(final String arg) {
 		if (!StringUtils.isEmpty(arg) && isParamEncoded(arg) && !org.apache.commons.lang3.StringUtils.isNumeric(arg)) {
+		    log.info("Decoding String param -> {}.", arg);
 			return new String(Base64.getDecoder().decode(arg));
 		} else {
+		    log.info("String param is NOT encoded!");
 			return arg;
 		}
 	}
 
 	public Long decodeStringParamAndConvertToLong(final String arg) {
 		if (!StringUtils.isEmpty(arg) && isParamEncoded(arg)) {
+            log.info("Decoding String param -> {} and converting to Long.", arg);
 			return new Long(new String(Base64.getDecoder().decode(arg)));
 		} else {
+            log.info("String param is NOT encoded!");
 			return new Long(arg);
 		}
 	}
 
-	private boolean isParamEncoded(final String param) {
-		return org.apache.tomcat.util.codec.binary.Base64.isBase64(param);
+	public boolean isParamEncoded(final String param) {
+	    return isBase64(param) && matchesBase64EncodedPattern(param) && isNotAlpha(param);
 	}
+
+	private boolean isBase64(final String param) {
+	    return org.apache.tomcat.util.codec.binary.Base64.isBase64(param);
+    }
+
+    private boolean matchesBase64EncodedPattern(final String param) {
+        final Pattern pattern = Pattern.compile(IS_BASE_64_ENCODED_PATTERN);
+        final Matcher matcher = pattern.matcher(param);
+        return matcher.matches();
+    }
+
+    private boolean isNotAlpha(final String param) {
+	    return !org.apache.commons.lang3.StringUtils.isAlpha(param);
+    }
 }
