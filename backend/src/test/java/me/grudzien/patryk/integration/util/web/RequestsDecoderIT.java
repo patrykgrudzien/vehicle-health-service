1a460d7eb7895bdf877fa53a6a0da6079afefac6
PatrykGrudzien <patryk.grudzien.91@gmail.com>
2018-12-03 11:20:42 +0100

    Removed Spring Test support in RequestsDecoderIT and implemented it as plain JUnit 5 test class.

--- backend/src/test/java/me/grudzien/patryk/integration/util/web/RequestsDecoderIT.java
+++ backend/src/test/java/me/grudzien/patryk/integration/util/web/RequestsDecoderIT.java
@@ -1,14 +1,7 @@
 package me.grudzien.patryk.integration.util.web;
 
 
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.context.annotation.Bean;
-import org.springframework.context.annotation.Configuration;
-import org.springframework.test.context.ContextConfiguration;
-import org.springframework.test.context.junit.jupiter.SpringExtension;
-
 import org.junit.jupiter.api.Assertions;
-import org.junit.jupiter.api.extension.ExtendWith;
 import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.Arguments;
 import org.junit.jupiter.params.provider.MethodSource;
@@ -22,26 +15,15 @@ import static org.assertj.core.api.Assertions.assertThat;
 import static org.junit.jupiter.api.Assertions.assertFalse;
 import static org.junit.jupiter.api.Assertions.assertTrue;
 
-@ExtendWith(SpringExtension.class)
-@ContextConfiguration(classes = RequestsDecoderIT.SmallTempContext.class)
 class RequestsDecoderIT {
 
-    @Configuration
-    static class SmallTempContext {
-
-        @Bean
-        public RequestsDecoder requestsDecoder() {
-            return new RequestsDecoder();
-        }
-    }
-
-    @Autowired
-    private RequestsDecoder requestsDecoder;
+    private final RequestsDecoder requestsDecoder = new RequestsDecoder();
 
     private static Stream<Arguments> inputParams() {
         return Stream.of(
                 Arguments.arguments("Patryk"),
                 Arguments.arguments("Grudzień"),
+                Arguments.arguments("ąźćżóęść"),
                 Arguments.arguments("John"),
                 Arguments.arguments("Snow"),
                 Arguments.arguments("test@email.com"),
