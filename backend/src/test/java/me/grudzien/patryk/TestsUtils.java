ffe9104939ad1111f08f11831d6544da8c901dd5
PatrykGrudzien <patryk.grudzien.91@gmail.com>
2018-12-04 16:58:31 +0100

    New methods inside TestsUtils.

--- backend/src/test/java/me/grudzien/patryk/TestsUtils.java
+++ backend/src/test/java/me/grudzien/patryk/TestsUtils.java
@@ -1,33 +1,105 @@
 package me.grudzien.patryk;
 
+import org.springframework.mobile.device.Device;
+import org.springframework.mobile.device.DevicePlatform;
+
 import org.apache.commons.lang3.RandomStringUtils;
 
 import com.fasterxml.jackson.core.JsonProcessingException;
 import com.fasterxml.jackson.databind.ObjectMapper;
+import com.google.common.collect.Sets;
 
 import java.util.Base64;
 import java.util.Base64.Encoder;
 import java.util.Optional;
 
 import me.grudzien.patryk.domain.dto.login.JwtAuthenticationRequest;
+import me.grudzien.patryk.domain.dto.login.JwtUser;
+import me.grudzien.patryk.domain.dto.registration.UserRegistrationDto;
+import me.grudzien.patryk.domain.dto.vehicle.VehicleDto;
+import me.grudzien.patryk.domain.entity.registration.Role;
+import me.grudzien.patryk.domain.enums.registration.RoleName;
+import me.grudzien.patryk.util.jwt.JwtUserFactory;
 
 public final class TestsUtils {
+
+    private static final Encoder encoder = Base64.getEncoder();
+    private static final ObjectMapper objectMapper = new ObjectMapper();
 
     private TestsUtils() {
         throw new UnsupportedOperationException("Creating object of this class is not allowed!");
     }
 
-    public static String prepareAuthJSONRequest(final String email, final String password, final boolean doEncoding) throws JsonProcessingException {
-        final Encoder encoder = Base64.getEncoder();
-        final ObjectMapper objectMapper = new ObjectMapper();
-
-        final String encodedEmail = Optional.ofNullable(email).map(notEmptyEmail -> encoder.encodeToString(notEmptyEmail.getBytes())).orElse(null);
-        final String encodedPassword = Optional.ofNullable(password).map(notEmptyPassword -> encoder.encodeToString(notEmptyPassword.getBytes())).orElse(null);
-
+    public static String prepareAuthJSONBody(final String email, final String password, final boolean doEncoding) throws JsonProcessingException {
         return objectMapper.writeValueAsString(JwtAuthenticationRequest.Builder()
-                                                                       .email(doEncoding ? encodedEmail : email)
-                                                                       .password(doEncoding ? encodedPassword : password)
+                                                                       .email(doEncoding ? encodeNotNullValue(email) : email)
+                                                                       .password(doEncoding ? encodeNotNullValue(password) : password)
                                                                        .refreshToken(RandomStringUtils.randomAlphanumeric(25))
                                                                        .build());
+    }
+
+    public static String prepareRefreshAccessTokenJSONBody() throws JsonProcessingException {
+        return objectMapper.writeValueAsString(JwtAuthenticationRequest.Builder()
+                                                                       .refreshToken(RandomStringUtils.randomAlphanumeric(25))
+                                                                       .build());
+    }
+
+    public static String prepareVehicleJSONBody(final String mileageToEncode) throws JsonProcessingException {
+        return objectMapper.writeValueAsString(VehicleDto.Builder()
+                                                         .encodedMileage(encodeNotNullValue(mileageToEncode))
+                                                         .build());
+    }
+
+    public static String prepareRegistrationJSONBody(final String firstName, final String lastName, final String email, final String password,
+                                                     final boolean doEncoding) throws JsonProcessingException {
+        return objectMapper.writeValueAsString(UserRegistrationDto.Builder()
+                                                                  .firstName(doEncoding ? encodeNotNullValue(firstName) : firstName)
+                                                                  .lastName(doEncoding ? encodeNotNullValue(lastName) : lastName)
+                                                                  .email(doEncoding ? encodeNotNullValue(email) : email)
+                                                                  .confirmedEmail(doEncoding ? encodeNotNullValue(email) : email)
+                                                                  .hasFakeEmail(true)
+                                                                  .password(doEncoding ? encodeNotNullValue(password) : password)
+                                                                  .confirmedPassword(doEncoding ? encodeNotNullValue(password) : password)
+                                                                  .build());
+    }
+
+    public static JwtUser prepareTestJwtUser() {
+        return JwtUser.Builder()
+                      .id(1L)
+                      .firstname("John")
+                      .lastname("Snow")
+                      .password("password")
+                      .email("test@email.com")
+                      .enabled(true)
+                      .roles(JwtUserFactory.mapRolesToAuthorities(Sets.newHashSet(new Role(RoleName.ROLE_USER))))
+                      .build();
+    }
+
+    public static Device testDevice() {
+        return new Device() {
+            @Override
+            public boolean isNormal() {
+                return true;
+            }
+
+            @Override
+            public boolean isMobile() {
+                return false;
+            }
+
+            @Override
+            public boolean isTablet() {
+                return false;
+            }
+
+            @Override
+            public DevicePlatform getDevicePlatform() {
+                return DevicePlatform.UNKNOWN;
+            }
+        };
+    }
+
+    private static String encodeNotNullValue(final String value) {
+        return Optional.ofNullable(value).map(notEmptyValue -> encoder.encodeToString(notEmptyValue.getBytes())).orElse(null);
     }
 }
