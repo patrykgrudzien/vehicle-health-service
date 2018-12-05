886453e2f006b753684c3296ad88a24aece4924d
patrykgrudzien <patryk.grudzien.91@gmail.com>
2018-12-05 14:55:21 +0100

    Removed blank line.

--- backend/src/test/java/me/grudzien/patryk/integration/util/web/RequestsDecoderIT.java
+++ backend/src/test/java/me/grudzien/patryk/integration/util/web/RequestsDecoderIT.java
@@ -1,6 +1,5 @@
 package me.grudzien.patryk.integration.util.web;
 
-
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.Arguments;
