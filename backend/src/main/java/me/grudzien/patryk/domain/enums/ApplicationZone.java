00554a53220e18e9b1c0c79026ffe2ea016f5996
PatrykGrudzien <patryk.grudzien.91@gmail.com>
2018-12-09 18:53:25 +0100

    Added Application Zone enum with helper methods.

---
+++ backend/src/main/java/me/grudzien/patryk/domain/enums/ApplicationZone.java
@@ -0,0 +1,27 @@
+package me.grudzien.patryk.domain.enums;
+
+import java.time.ZoneId;
+import java.time.ZonedDateTime;
+
+public enum ApplicationZone {
+
+	POLAND {
+		@Override
+		public String getZoneId() {
+			return ZoneId.getAvailableZoneIds()
+			             .stream()
+			             .filter(zoneId -> zoneId.equals("Poland"))
+			             .findFirst()
+			             .orElseThrow(() -> new RuntimeException("Cannot find \"Poland\" zoneId!"));
+		}
+
+        @Override
+        public ZonedDateTime getApplicationZonedDateTime() {
+            return ZonedDateTime.now(ZoneId.of(getZoneId()));
+        }
+    };
+
+	public abstract String getZoneId();
+
+	public abstract ZonedDateTime getApplicationZonedDateTime();
+}
