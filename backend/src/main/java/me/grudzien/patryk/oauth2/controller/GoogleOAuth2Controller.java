44fe7f7e8eb25e09154da66016854873b9b1a404
PatrykGrudzien <patryk.grudzien.91@gmail.com>
2019-01-04 19:33:27 +0100

    Added JwtTokenService dependency.

--- backend/src/main/java/me/grudzien/patryk/oauth2/controller/GoogleOAuth2Controller.java
+++ backend/src/main/java/me/grudzien/patryk/oauth2/controller/GoogleOAuth2Controller.java
@@ -17,13 +17,14 @@ import com.google.common.base.Preconditions;
 
 import javax.servlet.http.HttpServletResponse;
 
+import me.grudzien.patryk.domain.dto.login.JwtAuthenticationRequest;
 import me.grudzien.patryk.domain.dto.login.JwtAuthenticationResponse;
 import me.grudzien.patryk.domain.dto.login.JwtUser;
 import me.grudzien.patryk.domain.dto.responses.ExceptionResponse;
 import me.grudzien.patryk.domain.dto.responses.SuccessResponse;
 import me.grudzien.patryk.handler.web.HttpResponseHandler;
 import me.grudzien.patryk.oauth2.domain.CustomOAuth2OidcPrincipalUser.AccountStatus;
-import me.grudzien.patryk.util.jwt.JwtTokenUtil;
+import me.grudzien.patryk.service.jwt.JwtTokenService;
 
 import static me.grudzien.patryk.domain.enums.AppFLow.USER_LOGGED_IN_USING_GOOGLE;
 import static me.grudzien.patryk.oauth2.handler.CustomOAuth2AuthenticationSuccessHandler.SHORT_LIVED_AUTH_TOKEN_NAME;
@@ -34,12 +35,15 @@ import static me.grudzien.patryk.oauth2.handler.CustomOAuth2AuthenticationSucces
 public class GoogleOAuth2Controller {
 
 	private final HttpResponseHandler httpResponseHandler;
+	private final JwtTokenService jwtTokenService;
 
 	@Autowired
-	public GoogleOAuth2Controller(final HttpResponseHandler httpResponseHandler) {
-		Preconditions.checkNotNull(httpResponseHandler, "httpResponseHandler cannot be null!");
-		this.httpResponseHandler = httpResponseHandler;
-	}
+	public GoogleOAuth2Controller(final HttpResponseHandler httpResponseHandler, final JwtTokenService jwtTokenService) {
+        Preconditions.checkNotNull(httpResponseHandler, "httpResponseHandler cannot be null!");
+        Preconditions.checkNotNull(jwtTokenService, "jwtTokenService cannot be null!");
+        this.httpResponseHandler = httpResponseHandler;
+        this.jwtTokenService = jwtTokenService;
+    }
 
 	@GetMapping("${custom.properties.endpoints.oauth2.user-not-found}")
 	public ResponseEntity<ExceptionResponse> userNotFound() {
@@ -97,11 +101,15 @@ public class GoogleOAuth2Controller {
 	@GetMapping("${custom.properties.endpoints.oauth2.exchange-short-lived-token}")
 	public ResponseEntity<JwtAuthenticationResponse> exchangeShortLivedTokenForLonger(final Device device) {
 		final JwtUser jwtUser = (JwtUser) SecurityContextHolder.getContext().getAuthentication().getPrincipal();
-		return ResponseEntity.ok(JwtAuthenticationResponse.Builder()
-		                                                  .accessToken(JwtTokenUtil.Creator.generateAccessToken(jwtUser, device))
-		                                                  .refreshToken(JwtTokenUtil.Creator.generateRefreshToken(jwtUser))
-		                                                  .isSuccessful(Boolean.TRUE)
-		                                                  .build());
+        final JwtAuthenticationRequest jwtAuthenticationRequest = JwtAuthenticationRequest.Builder()
+                                                                                          .email(jwtUser.getEmail())
+                                                                                          .password(jwtUser.getPassword())
+                                                                                          .build();
+        return ResponseEntity.ok(JwtAuthenticationResponse.Builder()
+                                                          .accessToken(jwtTokenService.generateAccessToken(jwtAuthenticationRequest, device))
+                                                          .refreshToken(jwtTokenService.generateRefreshToken(jwtAuthenticationRequest, device))
+                                                          .isSuccessful(Boolean.TRUE)
+                                                          .build());
 	}
 
 	@GetMapping("${custom.properties.endpoints.oauth2.user-registered-using-google}")
