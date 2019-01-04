1288194a34458633debef029b12c12f2cb7016c8
PatrykGrudzien <patryk.grudzien.91@gmail.com>
2019-01-04 19:35:20 +0100

    Hide constructor for final class.

--- backend/src/main/java/me/grudzien/patryk/oauth2/util/OAuth2OidcAttributesExtractor.java
+++ backend/src/main/java/me/grudzien/patryk/oauth2/util/OAuth2OidcAttributesExtractor.java
@@ -2,7 +2,11 @@ package me.grudzien.patryk.oauth2.util;
 
 import java.util.Map;
 
-public class OAuth2OidcAttributesExtractor {
+public final class OAuth2OidcAttributesExtractor {
+
+    private OAuth2OidcAttributesExtractor() {
+        throw new UnsupportedOperationException("Creating object of this class is not allowed!");
+    }
 
 	public static String getOAuth2AttributeValue(final Map<String, Object> attributes, final String standardClaimName) {
 		return (String) attributes.get(standardClaimName);
