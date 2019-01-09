be64eb9f49f73917d4f30229b73f0879167a7a5e
patrykgrudzien <patryk.grudzien.91@gmail.com>
2019-01-09 11:34:35 +0100

    Added TokenType enum in JwtTokenConstants.

--- backend/src/main/java/me/grudzien/patryk/util/jwt/JwtTokenConstants.java
+++ backend/src/main/java/me/grudzien/patryk/util/jwt/JwtTokenConstants.java
@@ -2,15 +2,19 @@ package me.grudzien.patryk.util.jwt;
 
 public final class JwtTokenConstants {
 
-    static final String AUDIENCE_UNKNOWN = "unknown";
-    static final String AUDIENCE_WEB = "web";
-    static final String AUDIENCE_MOBILE = "mobile";
-    static final String AUDIENCE_TABLET = "tablet";
+    public static final String AUDIENCE_UNKNOWN = "unknown";
+    public static final String AUDIENCE_WEB = "web";
+    public static final String AUDIENCE_MOBILE = "mobile";
+    public static final String AUDIENCE_TABLET = "tablet";
 
     public static final String BEARER = "Bearer ";
     public static final int JWT_TOKEN_BEGIN_INDEX = 7;
 
     private JwtTokenConstants() {
         throw new UnsupportedOperationException("Creating object of this class is not allowed!");
+    }
+
+    public enum TokenType {
+    	ACCESS_TOKEN, REFRESH_TOKEN
     }
 }
