f044bf80e395cd144fa123786f1fef7cdedc901c
PatrykGrudzien <patryk.grudzien.91@gmail.com>
2019-01-12 22:00:06 +0100

    Improvements to registration unit test class.

--- backend/src/test/java/me/grudzien/patryk/unit/controller/registration/UserRegistrationControllerTest.java
+++ backend/src/test/java/me/grudzien/patryk/unit/controller/registration/UserRegistrationControllerTest.java
@@ -14,25 +14,27 @@ import org.springframework.test.web.servlet.result.MockMvcResultMatchers;
 import org.springframework.web.context.WebApplicationContext;
 
 import org.hamcrest.Matchers;
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
 import com.fasterxml.jackson.core.JsonProcessingException;
-import com.fasterxml.jackson.databind.ObjectMapper;
-
-import javax.validation.Validator;
 
 import me.grudzien.patryk.controller.registration.UserRegistrationController;
 import me.grudzien.patryk.domain.dto.registration.RegistrationResponse;
-import me.grudzien.patryk.domain.dto.registration.UserRegistrationDto;
 import me.grudzien.patryk.service.registration.UserRegistrationService;
 import me.grudzien.patryk.util.i18n.LocaleMessagesCreator;
-import me.grudzien.patryk.util.validator.CustomValidator;
 
 import static org.mockito.Mockito.any;
 import static org.mockito.Mockito.when;
 
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
+
+import static me.grudzien.patryk.TestsUtils.ENABLE_ENCODING;
+import static me.grudzien.patryk.TestsUtils.TEST_EMAIL;
+import static me.grudzien.patryk.TestsUtils.TEST_PASSWORD;
+import static me.grudzien.patryk.TestsUtils.prepareRegistrationJSONBody;
 
 @WebMvcTest(controllers = UserRegistrationController.class, secure = false)
 class UserRegistrationControllerTest {
@@ -43,20 +45,28 @@ class UserRegistrationControllerTest {
 	@Autowired
 	private WebApplicationContext webApplicationContext;
 
-	@MockBean
+	@SuppressWarnings("unused")
+    @MockBean
     private LocaleMessagesCreator localeMessagesCreator;
 
     @MockBean
 	private UserRegistrationService userRegistrationService;
 
-    private final Validator validator = CustomValidator.getDefaultValidator();
-    private final ObjectMapper objectMapper = new ObjectMapper();
+    private String registrationJSONBody;
 
     private static final String REGISTRATION_ENDPOINT = "/api/registration/register-user-account";
-    private static final String EMAIL = "test@email.com";
-    private static final String PASSWORD = "password";
 
-	@Test
+    @BeforeEach
+    void setUp() throws JsonProcessingException {
+        registrationJSONBody = prepareRegistrationJSONBody("John", "Snow", TEST_EMAIL, TEST_PASSWORD, ENABLE_ENCODING);
+    }
+
+    @AfterEach
+    void tearDown() {
+        registrationJSONBody = null;
+    }
+
+    @Test
     @DisplayName("Register user account - successful using MockMvc.")
 	void testRegisterUserAccountSuccessfully() throws Exception {
 		// given
@@ -72,7 +82,7 @@ class UserRegistrationControllerTest {
                 post(REGISTRATION_ENDPOINT)
                         .accept(MediaType.APPLICATION_JSON)
                         .contentType(MediaType.APPLICATION_JSON)
-                        .content(prepareRegistrationJSONRequest()))
+                        .content(registrationJSONBody))
                .andDo(MockMvcResultHandlers.print())
                .andExpect(MockMvcResultMatchers.status().isOk())
                .andExpect(MockMvcResultMatchers.jsonPath("$.message", Matchers.is("Registration success!")));
@@ -80,7 +90,7 @@ class UserRegistrationControllerTest {
 
     @Test
     @DisplayName("Register user account - successful using RestAssuredMockMvc.")
-    void testRegisterUserAccountSuccessfully_usingRestAssured() throws JsonProcessingException {
+    void testRegisterUserAccountSuccessfully_usingRestAssured() {
         // given
         final RegistrationResponse expectedResponse = RegistrationResponse.Builder()
                                                                           .message("Registration success!")
@@ -93,7 +103,7 @@ class UserRegistrationControllerTest {
         RestAssuredMockMvc.given()
                           .webAppContextSetup(webApplicationContext)
                           .log().all()
-                          .body(prepareRegistrationJSONRequest())
+                          .body(registrationJSONBody)
                           .contentType(ContentType.JSON)
                           .accept(ContentType.JSON)
                           .when()
@@ -102,16 +112,5 @@ class UserRegistrationControllerTest {
                           .log().all()
                           .statusCode(HttpStatus.OK.value())
                           .body("message", Matchers.is("Registration success!"));
-    }
-
-    private String prepareRegistrationJSONRequest() throws JsonProcessingException {
-	    return objectMapper.writeValueAsString(UserRegistrationDto.Builder()
-                                                                  .firstName("firstName")
-                                                                  .lastName("lastName")
-                                                                  .email(EMAIL)
-                                                                  .confirmedEmail(EMAIL)
-                                                                  .password(PASSWORD)
-                                                                  .confirmedPassword(PASSWORD)
-                                                                  .build());
     }
 }
