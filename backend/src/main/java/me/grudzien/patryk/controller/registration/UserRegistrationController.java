0b500665bebfb7ad53f173e5f362d57506e41524
PatrykGrudzien <patryk.grudzien.91@gmail.com>
2019-01-22 22:43:35 +0100

    Improvements to RegistrationController and new properties in RegistrationResponse class.

--- backend/src/main/java/me/grudzien/patryk/controller/registration/UserRegistrationController.java
+++ backend/src/main/java/me/grudzien/patryk/controller/registration/UserRegistrationController.java
@@ -3,6 +3,7 @@ package me.grudzien.patryk.controller.registration;
 import lombok.extern.log4j.Log4j2;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -15,14 +16,15 @@ import org.springframework.web.context.request.WebRequest;
 
 import com.google.common.base.Preconditions;
 
-import javax.servlet.http.HttpServletResponse;
+import java.net.URI;
 
 import me.grudzien.patryk.domain.dto.registration.RegistrationResponse;
 import me.grudzien.patryk.domain.dto.registration.UserRegistrationDto;
 import me.grudzien.patryk.domain.dto.responses.CustomResponse;
+import me.grudzien.patryk.domain.dto.responses.ExceptionResponse;
 import me.grudzien.patryk.domain.dto.responses.SuccessResponse;
 import me.grudzien.patryk.service.registration.UserRegistrationService;
-import me.grudzien.patryk.util.i18n.LocaleMessagesCreator;
+import me.grudzien.patryk.service.registration.event.RegistrationEventPublisher;
 
 @Log4j2
 @RestController
@@ -30,39 +32,46 @@ import me.grudzien.patryk.util.i18n.LocaleMessagesCreator;
 public class UserRegistrationController {
 
 	private final UserRegistrationService userRegistrationService;
-	private final LocaleMessagesCreator localeMessagesCreator;
+	private final RegistrationEventPublisher registrationEventPublisher;
 
 	@Autowired
 	public UserRegistrationController(final UserRegistrationService userRegistrationService,
-	                                  final LocaleMessagesCreator localeMessagesCreator) {
+                                      final RegistrationEventPublisher registrationEventPublisher) {
+        Preconditions.checkNotNull(userRegistrationService, "userRegistrationService cannot be null!");
+        Preconditions.checkNotNull(registrationEventPublisher, "registrationEventPublisher cannot be null!");
 
-		Preconditions.checkNotNull(userRegistrationService, "userRegistrationService cannot be null!");
-		Preconditions.checkNotNull(localeMessagesCreator, "localeMessagesCreator cannot be null!");
-
-		this.userRegistrationService = userRegistrationService;
-		this.localeMessagesCreator = localeMessagesCreator;
-	}
+        this.userRegistrationService = userRegistrationService;
+        this.registrationEventPublisher = registrationEventPublisher;
+    }
 
 	@PostMapping("${custom.properties.endpoints.registration.register-user-account}")
 	public ResponseEntity<CustomResponse> registerUserAccount(@RequestBody final UserRegistrationDto userRegistrationDto, final WebRequest webRequest) {
-        final RegistrationResponse registrationResponse = userRegistrationService.registerNewCustomUserAccount(userRegistrationDto, webRequest);
-        return registrationResponse.isSuccessful() ?
-                new ResponseEntity<>(new SuccessResponse(registrationResponse.getMessage()), HttpStatus.OK) :
-                ResponseEntity.badRequest().build();
+        final RegistrationResponse registrationResponse = userRegistrationService.registerNewCustomUserAccount(userRegistrationDto);
+        if (registrationResponse.isSuccessful()) {
+            final RegistrationResponse emailSent = registrationEventPublisher.publishRegistrationEven(registrationResponse.getRegisteredUser(), webRequest);
+            return emailSent.isSuccessful() ?
+                    ResponseEntity.ok().body(new SuccessResponse(emailSent.getMessage())) :
+                    ResponseEntity.badRequest().build();
+        }
+        return ResponseEntity.badRequest().body(new ExceptionResponse(registrationResponse.getMessage()));
 	}
 
 	@GetMapping("${custom.properties.endpoints.registration.confirm-registration}")
-	public ResponseEntity<CustomResponse> confirmRegistration(@RequestParam("token") final String token, final HttpServletResponse response,
-	                                                          @SuppressWarnings("unused") final WebRequest webRequest) {
-		userRegistrationService.confirmRegistration(token, response);
-		final String message = localeMessagesCreator.buildLocaleMessage("confirm-registration");
-		return new ResponseEntity<>(new SuccessResponse(message), HttpStatus.OK);
+	public ResponseEntity<CustomResponse> confirmRegistration(@RequestParam("token") final String token,
+                                                              @SuppressWarnings("unused") final WebRequest webRequest) {
+        final RegistrationResponse registrationResponse = userRegistrationService.confirmRegistration(token);
+		final HttpHeaders httpHeaders = new HttpHeaders();
+        httpHeaders.setLocation(URI.create(registrationResponse.getRedirectionUrl()));
+        final String responseMessage = registrationResponse.getMessage();
+        return registrationResponse.isSuccessful() ?
+                ResponseEntity.status(HttpStatus.MOVED_PERMANENTLY).headers(httpHeaders).body(new SuccessResponse(responseMessage)) :
+                ResponseEntity.badRequest().body(new ExceptionResponse(responseMessage));
 	}
 
 	@GetMapping("${custom.properties.endpoints.registration.resend-email-verification-token}")
 	public ResponseEntity<Void> resendEmailVerificationToken(@RequestParam("token") final String token,
 	                                                         @SuppressWarnings("unused") final WebRequest webRequest) {
-		userRegistrationService.resendEmailVerificationToken(token);
+//		userRegistrationService.resendEmailVerificationToken(token);
 		return new ResponseEntity<>(HttpStatus.OK);
 	}
 }
--- backend/src/main/java/me/grudzien/patryk/domain/dto/registration/RegistrationResponse.java
+++ backend/src/main/java/me/grudzien/patryk/domain/dto/registration/RegistrationResponse.java
@@ -4,23 +4,35 @@ import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
+import lombok.Setter;
 
 import com.fasterxml.jackson.annotation.JsonInclude;
 import com.fasterxml.jackson.annotation.JsonProperty;
 
 import java.io.Serializable;
 
+import me.grudzien.patryk.domain.entity.registration.CustomUser;
+
 @Getter
+@Setter
 @NoArgsConstructor
 @AllArgsConstructor
-@Builder(builderMethodName = "Builder")
+@Builder(builderMethodName = "hiddenBuilder")
 @JsonInclude(JsonInclude.Include.NON_NULL)
 public final class RegistrationResponse implements Serializable {
 
-    private static final long serialVersionUID = 4445835524695711394L;
+    private static final long serialVersionUID = 4183765640561510178L;
 
     private String message;
 
     @JsonProperty("successful")
     private boolean isSuccessful;
+
+    private String redirectionUrl;
+
+    private CustomUser registeredUser;
+
+    public static RegistrationResponseBuilder Builder(final boolean isSuccessful) {
+        return hiddenBuilder().isSuccessful(isSuccessful);
+    }
 }
