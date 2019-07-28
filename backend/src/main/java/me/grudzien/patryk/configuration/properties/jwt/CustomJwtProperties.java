f61102fd6c06fb386e5b6f9b3bc02e523c28df05
PatrykGrudzien <patryk.grudzien.91@gmail.com>
2019-07-28 22:45:40 +0200

    Project clean-up & refactoring #17

--- backend/src/main/java/me/grudzien/patryk/configuration/properties/jwt/CustomJwtProperties.java
+++ backend/src/main/java/me/grudzien/patryk/configuration/properties/jwt/CustomJwtProperties.java
@@ -4,11 +4,13 @@ import lombok.Getter;
 import lombok.Setter;
 
 import org.springframework.boot.context.properties.ConfigurationProperties;
+import org.springframework.context.annotation.Configuration;
 
 import static me.grudzien.patryk.configuration.properties.PrefixesDefinitions.CUSTOM_JWT_PROPERTIES_PREFIX;
 
 @Getter
 @Setter
+@Configuration
 @ConfigurationProperties(prefix = CUSTOM_JWT_PROPERTIES_PREFIX)
 public class CustomJwtProperties {
 
